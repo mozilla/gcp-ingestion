6d3dcd908e951ca8986e5ef6bbc6287281d990bd
Daniel Thorn <dthorn@mozilla.com>
2018-09-05 12:30:19 -0700

    Add ParseUserAgent to Validation

--- ingestion-beam/src/test/java/com/mozilla/telemetry/ValidateTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/ValidateTest.java
@@ -5,6 +5,8 @@
 package com.mozilla.telemetry;
 
 import com.mozilla.telemetry.transforms.GeoCityLookup;
+import com.mozilla.telemetry.transforms.GzipDecompress;
+import com.mozilla.telemetry.transforms.ParseUserAgent;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.beam.sdk.testing.PAssert;
@@ -20,32 +22,93 @@ public class ValidateTest {
   public final transient TestPipeline pipeline = TestPipeline.create();
 
   @Test
-  public void validate() {
+  public void gzipDecompress() {
     // Without setting this feature, the ordering of properties in output JSON is non-deterministic.
     Sink.objectMapper.configure(SerializationConfig.Feature.SORT_PROPERTIES_ALPHABETICALLY, true);
 
     final List<String> input = Arrays.asList(
         "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":{\"remote_addr\":\"8.8.8.8\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":"
-            + "{\"remote_addr\":\"10.0.0.2\""
-            + ",\"x_forwarded_for\":\"192.168.1.2, 63.245.208.195\""
-            + "},\"payload\":\"dGVzdA==\"}");
+        // TODO calculate this compression for the test
+        // payload="$(printf test | gzip -c | base64)"
+        "{\"payload\":\"H4sIAM1ekFsAAytJLS4BAAx+f9gEAAAA\"}");
 
     final List<String> expected = Arrays.asList(
         "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":{\"geo_country\":\"US\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":null,\"payload\":\"dGVzdA==\"}");
+
+    final PCollection<String> output = pipeline
+        .apply(Create.of(input))
+        .apply("decodeJson", Validate.decodeJson)
+        .get(Validate.decodeJson.mainTag)
+        .apply("gzipDecompress", new GzipDecompress())
+        .apply("encodeJson", Validate.encodeJson);
+
+    PAssert.that(output).containsInAnyOrder(expected);
+
+    pipeline.run();
+  }
+
+  @Test
+  public void geoCityLookup() {
+    // Without setting this feature, the ordering of properties in output JSON is non-deterministic.
+    Sink.objectMapper.configure(SerializationConfig.Feature.SORT_PROPERTIES_ALPHABETICALLY, true);
+
+    final List<String> input = Arrays.asList(
+        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":{\"remote_addr\":\"8.8.8.8\"},\"payload\":\"\"}",
+        "{\"attributeMap\":"
+            + "{\"remote_addr\":\"10.0.0.2\""
+            + ",\"x_forwarded_for\":\"192.168.1.2, 63.245.208.195\""
+            + "},\"payload\":\"\"}");
+
+    final List<String> expected = Arrays.asList(
+        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":{\"geo_country\":\"US\"},\"payload\":\"\"}",
         "{\"attributeMap\":"
             + "{\"geo_country\":\"US\""
             + ",\"geo_city\":\"Sacramento\""
             + ",\"geo_subdivision1\":\"CA\""
-            + "},\"payload\":\"dGVzdA==\"}");
+            + "},\"payload\":\"\"}");
 
     final PCollection<String> output = pipeline
         .apply(Create.of(input))
         .apply("decodeJson", Validate.decodeJson)
         .get(Validate.decodeJson.mainTag)
         .apply("geoCityLookup", new GeoCityLookup("GeoLite2-City.mmdb"))
+        .apply("encodeJson", Validate.encodeJson);
+
+    PAssert.that(output).containsInAnyOrder(expected);
+
+    pipeline.run();
+  }
+
+  @Test
+  public void parseUserAgent() {
+    // Without setting this feature, the ordering of properties in output JSON is non-deterministic.
+    Sink.objectMapper.configure(SerializationConfig.Feature.SORT_PROPERTIES_ALPHABETICALLY, true);
+
+    final List<String> input = Arrays.asList(
+        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":{\"user_agent\":\"\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":"
+            + "{\"user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0)"
+            + " Gecko/20100101 Firefox/63.0\""
+            + "},\"payload\":\"\"}");
+
+    final List<String> expected = Arrays.asList(
+        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":{},\"payload\":\"dGVzdA==\"}",
+        "{\"attributeMap\":"
+            + "{\"user_agent_browser\":\"Firefox\""
+            + ",\"user_agent_version\":\"63.0\""
+            + ",\"user_agent_os\":\"Macintosh\""
+            + "},\"payload\":\"\"}");
+
+    final PCollection<String> output = pipeline
+        .apply(Create.of(input))
+        .apply("decodeJson", Validate.decodeJson)
+        .get(Validate.decodeJson.mainTag)
+        .apply("parseUserAgent", new ParseUserAgent())
         .apply("encodeJson", Validate.encodeJson);
 
     PAssert.that(output).containsInAnyOrder(expected);
