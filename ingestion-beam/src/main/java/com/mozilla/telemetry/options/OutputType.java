b44f805de9d7f8592005af513c1ec64455f75a64
Jeff Klukas <jeff@klukas.net>
2018-09-07 12:50:03 -0400

    Factor out writing logic to be shared with ErrorOutputType

--- ingestion-beam/src/main/java/com/mozilla/telemetry/options/OutputType.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/options/OutputType.java
@@ -11,6 +11,7 @@ import com.mozilla.telemetry.transforms.CompositeTransform;
 import com.mozilla.telemetry.transforms.DecodePubsubMessages;
 import com.mozilla.telemetry.transforms.Foreach;
 import com.mozilla.telemetry.transforms.PubsubMessageToTableRow;
+import java.util.function.Consumer;
 import org.apache.beam.sdk.extensions.jackson.AsJsons;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
@@ -27,7 +28,7 @@ import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
 import org.apache.beam.sdk.values.PCollectionTuple;
-import org.joda.time.Duration;
+import org.apache.beam.sdk.values.PDone;
 
 /**
  * Enumeration of output types that each provide a {@code write} method.
@@ -38,21 +39,16 @@ import org.joda.time.Duration;
  */
 public enum OutputType {
   stdout {
-    /** Return a PTransform that prints messages to STDOUT; only for local running. */
+    /**
+     * Return a PTransform that prints messages to STDOUT; only for local running.
+     */
     public PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
         Sink.Options options
     ) {
-      return write(options.getOutputFileFormat());
-    }
-
-    protected PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
-        OutputFileFormat format
-    ) {
       return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
-        input
-            .apply(format.encode())
-            .apply(Foreach.string(System.out::println));
-        return input.apply(NO_ERRORS);
+        input.apply(
+            print(options.getOutputFileFormat(), System.out::println));
+        return input.apply(EMPTY_ERROR_COLLECTION);
       });
     }
   },
@@ -62,16 +58,10 @@ public enum OutputType {
     public PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
         Sink.Options options
     ) {
-      return write(options.getOutputFileFormat());
-    }
-
-    protected PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
-        OutputFileFormat format
-    ) {
-      return CompositeTransform.of((PCollection<PubsubMessage> input) -> { input
-          .apply(format.encode())
-          .apply(Foreach.string(System.err::println));
-        return input.apply(NO_ERRORS);
+      return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
+        input.apply(
+            print(options.getOutputFileFormat(), System.err::println));
+        return input.apply(EMPTY_ERROR_COLLECTION);
       });
     }
   },
@@ -81,21 +71,13 @@ public enum OutputType {
     public PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
         Sink.Options options
     ) {
-      Duration windowDuration = parseWindowDuration(options);
-      return write(options.getOutput(), options.getOutputFileFormat(), windowDuration);
-    }
-
-    protected PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
-        ValueProvider<String> outputPrefix,
-        OutputFileFormat format,
-        Duration windowDuration
-    ) {
       return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
-        input
-            .apply(format.encode())
-            .apply(Window.into(parseWindow(windowDuration)))
-            .apply(TextIO.write().to(outputPrefix).withWindowedWrites());
-        return input.apply(NO_ERRORS);
+        input.apply(
+            writeFiles(
+                options.getOutput(),
+                options.getOutputFileFormat(),
+                options.getWindowDuration()));
+        return input.apply(EMPTY_ERROR_COLLECTION);
       });
     }
   },
@@ -105,77 +87,105 @@ public enum OutputType {
     public PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
         Sink.Options options
     ) {
-      return write(options.getOutput());
-    }
-
-    protected PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
-        ValueProvider<String> location
-    ) {
       return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
-        input.apply(PubsubIO.writeMessages().to(location));
-        return input.apply(NO_ERRORS);
+        input.apply(
+            writePubsub(options.getOutput()));
+        return input.apply(EMPTY_ERROR_COLLECTION);
       });
     }
   },
 
   bigquery {
-    /**
-     * Return a PTransform that writes to a BigQuery table;
-     * also writes failed BigQuery inserts to the configured error output.
-     */
+    /** Return a PTransform that writes to a BigQuery table and collects failed inserts. */
     public PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
         Sink.Options options
     ) {
-      return write(options.getOutput());
-    }
-
-    protected PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
-        ValueProvider<String> tableSpec
-    ) {
-      return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
-        PubsubMessageToTableRow decodeTableRow = new PubsubMessageToTableRow();
-        AsJsons<TableRow> encodeTableRow = AsJsons.of(TableRow.class);
-        PCollectionTuple tableRows = input.apply(decodeTableRow);
-        final WriteResult writeResult = tableRows
-            .get(decodeTableRow.mainTag)
-            .apply(BigQueryIO
-                .writeTableRows()
-                .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
-                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
-                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
-                .to(tableSpec));
-        return PCollectionList
-            .of(tableRows
-                .get(decodeTableRow.errorTag))
-            .and(writeResult.getFailedInserts()
-                .apply(encodeTableRow)
-                .apply(InputFileFormat.text.decode()) // TODO: add error_{type,message} fields
-                .get(DecodePubsubMessages.mainTag))
-            .apply(Flatten.pCollections());
-      });
+      return writeBigQuery(options.getOutput());
     }
   };
 
+  /**
+   * Each case in the enum must implement this method to define how to write out messages.
+   *
+   * @return A PCollection of failure messages about data that could not be written.
+   */
   public abstract PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> write(
       Sink.Options options
   );
 
-  private static PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> NO_ERRORS =
+  /*
+   * Static methods giving concrete implementations of writing to various outputs.
+   * These are also accessed by ErrorOutputType.
+   */
+
+  protected static PTransform<PCollection<PubsubMessage>, PDone> print(
+      OutputFileFormat format,
+      Consumer<String> fn
+  ) {
+    return CompositeTransform.of(input -> input
+        .apply(format.encode())
+        .apply(Foreach.string(fn))
+    );
+  }
+
+  protected static PTransform<PCollection<PubsubMessage>, PDone> writeFiles(
+      ValueProvider<String> outputPrefix,
+      OutputFileFormat format,
+      String windowDuration
+  ) {
+    return CompositeTransform.of(input -> input
+        .apply(format.encode())
+        .apply(Window.into(parseWindow(windowDuration)))
+        .apply(TextIO.write().to(outputPrefix).withWindowedWrites())
+    );
+  }
+
+  protected static PTransform<PCollection<PubsubMessage>, PDone> writePubsub(
+      ValueProvider<String> location
+  ) {
+    return CompositeTransform.of(input -> input
+        .apply(PubsubIO.writeMessages().to(location))
+    );
+  }
+
+  protected static PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> writeBigQuery(
+      ValueProvider<String> tableSpec
+  ) {
+    return CompositeTransform.of((PCollection<PubsubMessage> input) -> {
+      PubsubMessageToTableRow decodeTableRow = new PubsubMessageToTableRow();
+      AsJsons<TableRow> encodeTableRow = AsJsons.of(TableRow.class);
+      PCollectionTuple tableRows = input.apply(decodeTableRow);
+      final WriteResult writeResult = tableRows
+          .get(decodeTableRow.mainTag)
+          .apply(BigQueryIO
+              .writeTableRows()
+              .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
+              .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
+              .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+              .to(tableSpec));
+      return PCollectionList
+          .of(tableRows
+              .get(decodeTableRow.errorTag))
+          .and(writeResult.getFailedInserts()
+              .apply(encodeTableRow)
+              .apply(InputFileFormat.text.decode()) // TODO: add error_{type,message} fields
+              .get(DecodePubsubMessages.mainTag))
+          .apply(Flatten.pCollections());
+    });
+  }
+
+  /*
+   * Public static fields and methods.
+   */
+
+  private static PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> EMPTY_ERROR_COLLECTION =
       CompositeTransform.of(input -> input
           .getPipeline()
           .apply(Create.empty(PubsubMessageWithAttributesCoder.of())
       ));
 
-  public static FixedWindows parseWindow(Duration duration) {
-    return FixedWindows.of(duration);
-  }
-
-  public static FixedWindows parseWindow(Sink.Options options) {
-    return FixedWindows.of(parseWindowDuration(options));
-  }
-
-  public static Duration parseWindowDuration(Sink.Options options) {
-    return DurationUtils.parseDuration(options.getWindowDuration());
+  public static FixedWindows parseWindow(String duration) {
+    return FixedWindows.of(DurationUtils.parseDuration(duration));
   }
 
 }
--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/FailureMessage.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/FailureMessage.java
@@ -5,17 +5,28 @@ import java.util.Arrays;
 import java.util.Map;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 
+/**
+ * A collection of static methods for producing consistently-formatted PubsubMessage
+ * packets for error output.
+ */
 public class FailureMessage {
-  public static PubsubMessage of(Object caller, String element, Throwable e) {
-    return FailureMessage.of(caller, element.getBytes(), e);
+
+  /**
+   * Return a PubsubMessage wrapping a String payload with attributes describing the error.
+   */
+  public static PubsubMessage of(Object caller, String payload, Throwable e) {
+    return FailureMessage.of(caller, payload.getBytes(), e);
   }
 
-  public static PubsubMessage of(Object caller, byte[] element, Throwable e) {
+  /**
+   * Return a PubsubMessage wrapping a byte array payload with attributes describing the error.
+   */
+  public static PubsubMessage of(Object caller, byte[] payload, Throwable e) {
     Map<String, String> attributes = ImmutableMap.of(
         "error_type", caller.toString(),
         "error_message", e.toString(),
         "stack_trace", Arrays.toString(e.getStackTrace())
     );
-    return new PubsubMessage(element, attributes);
+    return new PubsubMessage(payload, attributes);
   }
 }
