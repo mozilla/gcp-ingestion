87154e45ea81b2bfbd8dae7bdb3e01aab0a2fca8
Jeff Klukas <jeff@klukas.net>
2018-09-07 16:11:04 -0400

    Move Decoder-specific transforms to separate package (#51)

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/GeoCityLookup.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/GeoCityLookup.java
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.mozilla.telemetry.transforms;
+package com.mozilla.telemetry.decoder;
 
 import com.maxmind.db.CHMCache;
 import com.maxmind.geoip2.DatabaseReader;
--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUserAgent.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUserAgent.java
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.mozilla.telemetry.transforms;
+package com.mozilla.telemetry.decoder;
 
 import com.google.common.collect.Sets;
 import java.io.Serializable;
