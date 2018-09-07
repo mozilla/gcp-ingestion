70f9601946cbd57412284fef875951c787d1cafb
Jeff Klukas <jeff@klukas.net>
2018-09-07 13:40:24 -0400

    Restore logic of adding error attributes to a failed PubsubMessage

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/FailureMessage.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/FailureMessage.java
@@ -1,8 +1,8 @@
 package com.mozilla.telemetry.transforms;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.collect.ImmutableMap;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 
@@ -13,15 +13,12 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 public class FailureMessage {
 
   /**
-   * Return a PubsubMessage wrapping a String payload with attributes describing the error.
+   * Return a modified PubsubMessage with additional attributes describing the error.
    */
-  public static PubsubMessage of(Object caller, PubsubMessage payload, Throwable e) {
-    try {
-      return FailureMessage.of(caller, PubsubMessageMixin.MAPPER.writeValueAsBytes(payload), e);
-    } catch (JsonProcessingException jsonException) {
-      throw new RuntimeException(
-          "Unexpected failure to encode a PubsubMessage to bytes", jsonException);
-    }
+  public static PubsubMessage of(Object caller, PubsubMessage message, Throwable e) {
+    final Map<String, String> attributes = new HashMap<>(message.getAttributeMap());
+    attributes.putAll(errorAttributes(caller, e));
+    return new PubsubMessage(message.getPayload(), attributes);
   }
 
   /**
@@ -35,11 +32,14 @@ public class FailureMessage {
    * Return a PubsubMessage wrapping a byte array payload with attributes describing the error.
    */
   public static PubsubMessage of(Object caller, byte[] payload, Throwable e) {
-    Map<String, String> attributes = ImmutableMap.of(
+    return new PubsubMessage(payload, errorAttributes(caller, e));
+  }
+
+  private static Map<String, String> errorAttributes(Object caller, Throwable e) {
+    return ImmutableMap.of(
         "error_type", caller.toString(),
         "error_message", e.toString(),
         "stack_trace", Arrays.toString(e.getStackTrace())
     );
-    return new PubsubMessage(payload, attributes);
   }
 }
