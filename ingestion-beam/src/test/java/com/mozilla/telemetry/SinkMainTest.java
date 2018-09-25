36bcb49dea18699d9389121828564d453be555cf
Jeff Klukas <jeff@klukas.net>
2018-09-25 09:27:10 -0400

    Add SinkMain tests for placeholders and text<->Json
    Partially addresses #74

--- ingestion-beam/src/test/java/com/mozilla/telemetry/SinkMainTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/SinkMainTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertThat;
 
 import com.google.common.io.Resources;
 import com.mozilla.telemetry.matchers.Lines;
+import java.util.Collections;
 import java.util.List;
 import org.apache.beam.sdk.Pipeline.PipelineExecutionException;
 import org.hamcrest.Matchers;
@@ -58,11 +59,41 @@ public class SinkMainTest {
   }
 
   @Test
+  public void testAttributePlaceholders() {
+    String inputPath = Resources.getResource("testdata/attribute-placeholders").getPath();
+    String input = inputPath + "/*.ndjson";
+    String output = outputPath + "/${host:-no_host}/${geo_city}/out";
+
+    Sink.main(new String[]{
+        "--inputFileFormat=json",
+        "--inputType=file",
+        "--input=" + input,
+        "--outputFileFormat=json",
+        "--outputType=file",
+        "--output=" + output
+    });
+
+    assertThat(Lines.files(outputPath + "/**/out*.ndjson"), matchesInAnyOrder(
+               Lines.files(inputPath + "/*.ndjson")));
+
+    assertThat(Lines.files(outputPath + "/hostA/cityA/out*.ndjson"), matchesInAnyOrder(
+               Lines.files(inputPath + "/hostA-cityA.ndjson")));
+
+    assertThat(Lines.files(outputPath + "/hostA/cityB/out*.ndjson"), matchesInAnyOrder(
+               Lines.files(inputPath + "/hostA-cityB.ndjson")));
+
+    assertThat(Lines.files(outputPath + "/no_host/cityA/out*.ndjson"), matchesInAnyOrder(
+               Lines.files(inputPath + "/null-cityA.ndjson")));
+
+    assertThat(Lines.files(outputPath + "/no_host/cityB/out*.ndjson"), matchesInAnyOrder(
+        Collections.emptyList()));
+  }
+
+  @Test
   public void canWriteSingleRecord() {
     String input = Resources.getResource("testdata/single-message-input.json").getPath();
     String output = outputPath + "/out";
 
-    // We are simply making sure this runs without throwing an exception.
     Sink.main(new String[]{
         "--inputFileFormat=json",
         "--inputType=file",
@@ -73,7 +104,7 @@ public class SinkMainTest {
     });
 
     List<String> inputLines = Lines.files(input);
-    List<String> outputLines = Lines.files(outputPath + "/*.ndjson");
+    List<String> outputLines = Lines.files(outputPath + "/out*.ndjson");
 
     assertThat(outputLines, matchesInAnyOrder(inputLines));
   }
@@ -84,7 +115,6 @@ public class SinkMainTest {
     String output = outputPath + "/valid/out";
     String errorOutput = outputPath + "/error/out";
 
-    // We are simply making sure this runs without throwing an exception.
     Sink.main(new String[]{
         "--inputFileFormat=json",
         "--inputType=file",
@@ -99,11 +129,51 @@ public class SinkMainTest {
     List<String> validLines = Lines.resources("testdata/basic-messages-valid.ndjson");
     List<String> invalidLines = Lines.resources("testdata/basic-messages-invalid.ndjson");
 
-    List<String> outputLines = Lines.files(outputPath + "/valid/*.ndjson");
-    List<String> errorOutputLines = Lines.files(outputPath + "/error/*.ndjson");
+    List<String> outputLines = Lines.files(outputPath + "/valid/out*.ndjson");
+    List<String> errorOutputLines = Lines.files(outputPath + "/error/out*.ndjson");
 
     assertThat(outputLines, matchesInAnyOrder(validLines));
     assertThat(errorOutputLines, Matchers.hasSize(invalidLines.size()));
+  }
+
+  @Test
+  public void testTextToJson() {
+    String input = Resources.getResource("testdata/basic-messages-payloads.txt").getPath();
+    String output = outputPath + "/out";
+
+    Sink.main(new String[]{
+        "--inputFileFormat=text",
+        "--inputType=file",
+        "--input=" + input,
+        "--outputFileFormat=json",
+        "--outputType=file",
+        "--output=" + output
+    });
+
+    List<String> outputLines = Lines.files(outputPath + "/out*.ndjson");
+    List<String> expected = Lines.resources("testdata/basic-messages-valid-null-attributes.ndjson");
+
+    assertThat(outputLines, matchesInAnyOrder(expected));
+  }
+
+  @Test
+  public void testJsonToText() {
+    String input = Resources.getResource("testdata/basic-messages-valid.ndjson").getPath();
+    String output = outputPath + "/out";
+
+    Sink.main(new String[]{
+        "--inputFileFormat=json",
+        "--inputType=file",
+        "--input=" + input,
+        "--outputFileFormat=text",
+        "--outputType=file",
+        "--output=" + output
+    });
+
+    List<String> outputLines = Lines.files(outputPath + "/out*.txt");
+    List<String> expected = Lines.resources("testdata/basic-messages-payloads.txt");
+
+    assertThat(outputLines, matchesInAnyOrder(expected));
   }
 
 }
