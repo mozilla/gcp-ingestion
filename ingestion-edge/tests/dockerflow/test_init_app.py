6abfde25eddb5b08339a8a6ec9a12e732f878c20
Daniel Thorn <dthorn@mozilla.com>
2018-11-16 08:55:34 -0800

    Bring ingestion-edge unit test coverage to 100% (#200)

---
+++ ingestion-edge/tests/dockerflow/test_init_app.py
@@ -0,0 +1,62 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+from ingestion_edge.create_app import create_app
+from sanic.request import Request
+from typing import Any, Dict, List
+import pytest
+import json
+
+
+class Config:
+    METADATA_HEADERS: Dict[str, str] = {}
+    ROUTE_TABLE: List[Any] = []
+
+
+@pytest.fixture
+def app(mocker):
+    mocker.patch("ingestion_edge.publish.SQLiteAckQueue", dict)
+    mocker.patch("ingestion_edge.dockerflow.check_disk_bytes_free", lambda app: [])
+    mocker.patch("ingestion_edge.create_app.config", Config)
+    app = create_app()
+    return app
+
+
+async def test_heartbeat(app):
+    responses = []
+    request = Request(b"/__heartbeat__", {}, "1.1", "GET", "http")
+    await app.handle_request(request, lambda r: responses.append(r), None)
+    assert len(responses) == 1
+    response = responses.pop()
+    assert response.status == 200
+    assert json.loads(response.body.decode()) == {
+        "status": "ok",
+        "checks": {"check_disk_bytes_free": "ok"},
+        "details": {},
+    }
+
+
+async def test_lbheartbeat(app):
+    responses = []
+    request = Request(b"/__lbheartbeat__", {}, "1.1", "GET", "http")
+    await app.handle_request(request, lambda r: responses.append(r), None)
+    assert len(responses) == 1
+    response = responses.pop()
+    assert response.status == 200
+    assert response.body == b""
+
+
+async def test_version(app):
+    responses = []
+    request = Request(b"/__version__", {}, "1.1", "GET", "http")
+    await app.handle_request(request, lambda r: responses.append(r), None)
+    assert len(responses) == 1
+    response = responses.pop()
+    assert response.status == 200
+    assert set(json.loads(response.body.decode()).keys()) == {
+        "build",
+        "commit",
+        "source",
+        "version",
+    }
