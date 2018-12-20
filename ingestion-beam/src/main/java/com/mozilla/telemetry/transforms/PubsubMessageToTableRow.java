f2c44bed806600ab2b867878714d2e57bd6c1df7
Jeff Klukas <jeff@klukas.net>
2018-12-19 21:32:04 -0500

    Dynamic destinations for BigQuery output
    Closes #291

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/PubsubMessageToTableRow.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/PubsubMessageToTableRow.java
@@ -5,13 +5,57 @@
 package com.mozilla.telemetry.transforms;
 
 import com.google.api.services.bigquery.model.TableRow;
+import com.google.common.collect.Maps;
 import com.mozilla.telemetry.util.Json;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.beam.sdk.coders.KvCoder;
+import org.apache.beam.sdk.io.gcp.bigquery.TableDestination;
+import org.apache.beam.sdk.io.gcp.bigquery.TableDestinationCoderV2;
+import org.apache.beam.sdk.io.gcp.bigquery.TableRowJsonCoder;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.commons.text.StringSubstitutor;
 
-public class PubsubMessageToTableRow extends MapElementsWithErrors<PubsubMessage, TableRow> {
+public class PubsubMessageToTableRow
+    extends MapElementsWithErrors<PubsubMessage, KV<TableDestination, TableRow>> {
 
-  protected TableRow processElement(PubsubMessage element) throws IOException {
-    return Json.readTableRow(element.getPayload());
+  public static PubsubMessageToTableRow of(ValueProvider<String> tableSpecTemplate) {
+    return new PubsubMessageToTableRow(tableSpecTemplate);
+  }
+
+  private final ValueProvider<String> tableSpecTemplate;
+
+  private PubsubMessageToTableRow(ValueProvider<String> tableSpecTemplate) {
+    this.tableSpecTemplate = tableSpecTemplate;
+  }
+
+  protected KV<TableDestination, TableRow> processElement(PubsubMessage element)
+      throws IOException {
+    Map<String, String> attributes = Optional.ofNullable(element.getAttributeMap()) //
+        // Only letters, numbers, and underscores are allowed in BigQuery dataset and table names,
+        // but some doc types and namespaces contain '-', so we convert to '_'.
+        .map(m -> Maps.transformValues(m, v -> v.replaceAll("-", "_"))).orElse(new HashMap<>());
+    String tableSpec = StringSubstitutor.replace(tableSpecTemplate.get(), attributes);
+    if (tableSpec.contains("$")) {
+      throw new IllegalArgumentException("Element did not contain all the attributes needed to"
+          + " fill out variables in the configured BigQuery output template: "
+          + tableSpecTemplate.get());
+    }
+    TableDestination tableDestination = new TableDestination(tableSpec, null);
+    TableRow tableRow = Json.readTableRow(element.getPayload());
+    return KV.of(tableDestination, tableRow);
+  }
+
+  @Override
+  public ResultWithErrors<PCollection<KV<TableDestination, TableRow>>> expand(
+      PCollection<? extends PubsubMessage> input) {
+    ResultWithErrors<PCollection<KV<TableDestination, TableRow>>> result = super.expand(input);
+    result.output().setCoder(KvCoder.of(TableDestinationCoderV2.of(), TableRowJsonCoder.of()));
+    return result;
   }
 }
