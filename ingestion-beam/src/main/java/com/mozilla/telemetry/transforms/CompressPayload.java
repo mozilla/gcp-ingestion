368a59f474800303698e8d871cd3f545288c1b18
Jeff Klukas <jeff@klukas.net>
2019-02-08 16:36:35 -0500

    Truncate error payloads too large for Pubsub

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/CompressPayload.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/CompressPayload.java
@@ -12,8 +12,11 @@ import java.io.IOException;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.Arrays;
 import org.apache.beam.sdk.io.Compression;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
@@ -24,38 +27,55 @@ public class CompressPayload
     extends PTransform<PCollection<? extends PubsubMessage>, PCollection<PubsubMessage>> {
 
   public static CompressPayload of(ValueProvider<Compression> compression) {
-    return new CompressPayload(compression);
+    return new CompressPayload(compression, Integer.MAX_VALUE);
+  }
+
+  public CompressPayload withMaxCompressedBytes(int maxBytes) {
+    return new CompressPayload(this.compression, maxBytes);
   }
 
   @Override
   public PCollection<PubsubMessage> expand(PCollection<? extends PubsubMessage> input) {
-    return input.apply(MapElements.into(TypeDescriptor.of(PubsubMessage.class))
-        .via(message -> compress(message, compression.get())));
+    return input
+        .apply(MapElements.into(TypeDescriptor.of(PubsubMessage.class)).via(this::compress));
   }
 
   ////////
 
-  private final ValueProvider<Compression> compression;
+  final ValueProvider<Compression> compression;
+  private final int maxCompressedBytes;
 
-  private CompressPayload(ValueProvider<Compression> compression) {
+  private final Counter truncationCounter = Metrics.counter(CompressPayload.class,
+      "truncated_payload");
+
+  private CompressPayload(ValueProvider<Compression> compression, int maxCompressedBytes) {
     this.compression = compression;
+    this.maxCompressedBytes = maxCompressedBytes;
   }
 
   @VisibleForTesting
-  static PubsubMessage compress(PubsubMessage message, Compression compression) {
-    if (message == null || message.getPayload() == null) {
-      return message;
+  PubsubMessage compress(PubsubMessage message) {
+    message = PubsubConstraints.ensureNonNull(message);
+    byte[] compressedBytes = compress(message.getPayload(), compression.get());
+    if (compressedBytes.length > maxCompressedBytes) {
+      byte[] truncated = Arrays.copyOfRange(message.getPayload(), 0, maxCompressedBytes);
+      truncationCounter.inc();
+      compressedBytes = compress(truncated, compression.get());
     }
-    byte[] bytes = message.getPayload();
+    return new PubsubMessage(compressedBytes, message.getAttributeMap());
+  }
+
+  @VisibleForTesting
+  static byte[] compress(byte[] payload, Compression compression) {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     // We use a try-with-resources statement to ensure everything gets closed appropriately.
-    try (ReadableByteChannel inChannel = Channels.newChannel(new ByteArrayInputStream(bytes));
+    try (ReadableByteChannel inChannel = Channels.newChannel(new ByteArrayInputStream(payload));
         WritableByteChannel outChannel = compression.writeCompressed(Channels.newChannel(out))) {
       ByteStreams.copy(inChannel, outChannel);
     } catch (IOException e) {
-      return message;
+      return payload;
     }
-    return new PubsubMessage(out.toByteArray(), message.getAttributeMap());
+    return out.toByteArray();
   }
 
 }
