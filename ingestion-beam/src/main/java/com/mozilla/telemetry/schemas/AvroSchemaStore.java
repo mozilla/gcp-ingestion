cc10db3020e0e490dfde625be2d03d33e10dbec8
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-02-20 11:47:20 -0800

    Refactor SchemaStore for derived classes

---
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/schemas/AvroSchemaStore.java
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.mozilla.telemetry.schemas;
+
+import java.io.IOException;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.avro.Schema;
+
+public class AvroSchemaStore extends SchemaStore<Schema> {
+
+  /** Returns a SchemaStore based on the contents of the archive at schemasLocation. */
+  public static AvroSchemaStore of(ValueProvider<String> schemasLocation) {
+    return new AvroSchemaStore(schemasLocation);
+  }
+
+  protected AvroSchemaStore(ValueProvider<String> schemasLocation) {
+    super(schemasLocation);
+  }
+
+  @Override
+  protected boolean containsSchemaSuffix(String name) {
+    return name.endsWith(".avro.json");
+  }
+
+  @Override
+  protected Schema loadSchemaFromArchive(ArchiveInputStream archive) throws IOException {
+    Schema.Parser parser = new Schema.Parser();
+    return parser.parse(archive);
+  }
+}
---
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/schemas/JSONSchemaStore.java
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.mozilla.telemetry.schemas;
+
+import com.mozilla.telemetry.util.Json;
+import java.io.IOException;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.io.IOUtils;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+
+public class JSONSchemaStore extends SchemaStore<Schema> {
+
+  /** Returns a SchemaStore based on the contents of the archive at schemasLocation. */
+  public static JSONSchemaStore of(ValueProvider<String> schemasLocation) {
+    return new JSONSchemaStore(schemasLocation);
+  }
+
+  protected JSONSchemaStore(ValueProvider<String> schemasLocation) {
+    super(schemasLocation);
+  }
+
+  @Override
+  protected boolean containsSchemaSuffix(String name) {
+    return name.endsWith(".schema.json");
+  }
+
+  @Override
+  protected Schema loadSchemaFromArchive(ArchiveInputStream archive) throws IOException {
+    byte[] bytes = IOUtils.toByteArray(archive);
+    JSONObject json = Json.readJSONObject(bytes);
+    return SchemaLoader.load(json);
+  }
+}
