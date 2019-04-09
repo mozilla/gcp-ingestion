e4bfdd75a7d2e15cc4c524fe3260b045dc51e8e4
Jeff Klukas <jeff@klukas.net>
2019-04-09 16:03:27 -0400

    Calculate sample_id in the Decoder

---
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/republisher/RandomSampler.java
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.mozilla.telemetry.republisher;
+
+import java.util.concurrent.ThreadLocalRandom;
+import javax.annotation.Nullable;
+
+public class RandomSampler {
+
+  /**
+   * If the given sampleIdString is convertible to an integer between 0 and 99,
+   * return whether the integer is less than {@code int(100 * ratio)}.
+   * Otherwise, generate a random number between 0 and 1 and return whether the generated number
+   * is less then the passed ratio.
+   */
+  public static boolean filterBySampleIdOrRandomNumber(@Nullable String sampleIdString,
+      Double ratio) {
+    Long sampleId = null;
+    if (sampleIdString != null) {
+      try {
+        sampleId = Long.valueOf(sampleIdString);
+      } catch (NumberFormatException ignore) {
+        // pass
+      }
+    }
+    return filterBySampleIdOrRandomNumber(sampleId, ratio);
+  }
+
+  /**
+   * If the given sampleId is between 0 and 99,
+   * return whether the integer is less than {@code int(100 * ratio)}.
+   * Otherwise, generate a random number between 0 and 1 and return whether the generated number
+   * is less then the passed ratio.
+   */
+  public static boolean filterBySampleIdOrRandomNumber(@Nullable Long sampleId, Double ratio) {
+    if (sampleId != null && sampleId >= 0 && sampleId < 100) {
+      long maxPercentile = Math.round(ratio * 100);
+      return sampleId < maxPercentile;
+    } else {
+      return ThreadLocalRandom.current().nextDouble() < ratio;
+    }
+  }
+
+}
