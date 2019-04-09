854f5bd050f98b9edbd713ea56025c38df8a98a3
Jeff Klukas <jeff@klukas.net>
2019-04-09 16:03:27 -0400

    Calculate sample_id in the Decoder

---
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/republisher/RandomSamplerTest.java
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.mozilla.telemetry.republisher;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RandomSamplerTest {
+
+  @Test
+  public void filterBySampleIdOrRandomNumber() {
+    assertTrue(RandomSampler.filterBySampleIdOrRandomNumber(0L, 0.01));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(1L, 0.01));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(99L, 0.01));
+
+    assertTrue(RandomSampler.filterBySampleIdOrRandomNumber(0L, 0.05));
+    assertTrue(RandomSampler.filterBySampleIdOrRandomNumber(4L, 0.05));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(5L, 0.05));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(99L, 0.05));
+
+    assertTrue(RandomSampler.filterBySampleIdOrRandomNumber(99L, 1.0));
+
+    assertTrue(RandomSampler.filterBySampleIdOrRandomNumber("0", 0.01));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber("1", 0.01));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber("99", 0.01));
+
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(-3L, 0.00));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber(400L, 0.00));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber("foo", 0.00));
+    assertFalse(RandomSampler.filterBySampleIdOrRandomNumber((String) null, 0.00));
+  }
+}
