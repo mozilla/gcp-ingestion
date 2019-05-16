c357d79eca07716fa8ef537d5413d2f415dce1d0
Daniel Thorn <dthorn@mozilla.com>
2019-05-16 12:17:38 -0700

    Implement user agent parsing with exact aws behavior (#604)
    * Implement user agent parsing with exact aws behavior
    
    fixes #556
    
    * Address review
    
    * fix typo
    
    * fix spotless
    
    * fix indent
    
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>
    
    * adjust indent
    
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>
    
    * fix test name
    
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>
    
    * fix indent
    
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>

--- ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/ParseUserAgentTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/ParseUserAgentTest.java
@@ -4,10 +4,15 @@
 
 package com.mozilla.telemetry.decoder;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.mozilla.telemetry.options.InputFileFormat;
 import com.mozilla.telemetry.options.OutputFileFormat;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
@@ -20,21 +25,72 @@ public class ParseUserAgentTest {
   @Rule
   public final transient TestPipeline pipeline = TestPipeline.create();
 
-  @Test
-  public void testOutput() {
-    final List<String> input = Arrays.asList(
-        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":{\"user_agent\":\"\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":"
-            + "{\"user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0)"
-            + " Gecko/20100101 Firefox/63.0\"" + "},\"payload\":\"\"}");
+  private static final String BROWSER = "user_agent_browser";
+  private static final String OS = "user_agent_os";
+  private static final String VERSION = "user_agent_version";
 
-    final List<String> expected = Arrays.asList(
-        "{\"attributeMap\":{\"host\":\"test\"},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":{},\"payload\":\"dGVzdA==\"}",
-        "{\"attributeMap\":" + "{\"user_agent_browser\":\"Firefox\""
-            + ",\"user_agent_version\":\"63.0\"" + ",\"user_agent_os\":\"Macintosh\""
-            + "},\"payload\":\"\"}");
+  private static final Map<String, Map<String, String>> osSingleMatch = new ImmutableMap //
+      .Builder<String, Map<String, String>>()
+          .put("Firefox AndroidSync", ImmutableMap.of(BROWSER, "FxSync", OS, "Android"))
+          .put("Firefox-iOS-Sync", ImmutableMap.of(BROWSER, "FxSync", OS, "iOS"))
+          .put("iPod", ImmutableMap.of(OS, "iPod")) //
+          .put("iPad", ImmutableMap.of(OS, "iPad")) //
+          .put("iPhone", ImmutableMap.of(OS, "iPhone"))
+          .put("Android", ImmutableMap.of(OS, "Android"))
+          .put("BlackBerry", ImmutableMap.of(OS, "BlackBerry"))
+          .put("Macintosh", ImmutableMap.of(OS, "Macintosh"))
+          .put("Mozilla/5.0 (Mobile;", ImmutableMap.of(OS, "FirefoxOS"))
+          .put("Windows NT 10.0", ImmutableMap.of(OS, "Windows 10"))
+          .put("Windows NT 6.3", ImmutableMap.of(OS, "Windows 8.1"))
+          .put("Windows NT 6.2", ImmutableMap.of(OS, "Windows 8"))
+          .put("Windows NT 6.1", ImmutableMap.of(OS, "Windows 7"))
+          .put("Windows NT 6.0", ImmutableMap.of(OS, "Windows Vista"))
+          .put("Windows NT 5.1", ImmutableMap.of(OS, "Windows XP"))
+          .put("Windows NT 5.0", ImmutableMap.of(OS, "Windows 2000")) //
+          .build();
+
+  private static final List<String> osPriority = new ArrayList<>(osSingleMatch.keySet());
+  private static final List<List<String>> osBestMatch = IntStream.range(0, osPriority.size() - 1)
+      .mapToObj(i -> osPriority.subList(i, osPriority.size() - 1)).collect(Collectors.toList());
+
+  private static final Map<String, Map<String, String>> browserSingleMatch = new ImmutableMap //
+      .Builder<String, Map<String, String>>()
+          .put("Edge/1", ImmutableMap.of(BROWSER, "Edge", VERSION, "1"))
+          .put("Edge", ImmutableMap.of(BROWSER, "Edge"))
+          .put("Chrome/2", ImmutableMap.of(BROWSER, "Chrome", VERSION, "2"))
+          .put("Chrome", ImmutableMap.of(BROWSER, "Chrome"))
+          .put("Opera Mini", ImmutableMap.of(BROWSER, "Opera Mini"))
+          .put("Opera Mobi", ImmutableMap.of(BROWSER, "Opera Mobi"))
+          .put("Opera", ImmutableMap.of(BROWSER, "Opera"))
+          .put("MSIE 3", ImmutableMap.of(BROWSER, "MSIE", VERSION, "3"))
+          .put("MSIE", ImmutableMap.of(BROWSER, "MSIE"))
+          .put("Trident/7.0", ImmutableMap.of(BROWSER, "MSIE", VERSION, "11"))
+          .put("Safari", ImmutableMap.of(BROWSER, "Safari"))
+          .put("Firefox AndroidSync 4",
+              ImmutableMap.of(BROWSER, "FxSync", VERSION, "4", OS, "Android"))
+          .put("Firefox AndroidSync", ImmutableMap.of(BROWSER, "FxSync", OS, "Android"))
+          .put("Firefox-iOS-Sync/5", ImmutableMap.of(BROWSER, "FxSync", VERSION, "5", OS, "iOS"))
+          .put("Firefox-iOS-Sync", ImmutableMap.of(BROWSER, "FxSync", OS, "iOS"))
+          .put("Firefox/6", ImmutableMap.of(BROWSER, "Firefox", VERSION, "6"))
+          .put("Firefox", ImmutableMap.of(BROWSER, "Firefox")) //
+          .build();
+
+  private static final List<String> browserPriority = new ArrayList<>(browserSingleMatch.keySet());
+  private static final List<List<String>> browserBestMatch = IntStream
+      .range(0, browserPriority.size() - 1)
+      .mapToObj(i -> browserPriority.subList(i, browserPriority.size() - 1))
+      .collect(Collectors.toList());
+
+  private void test(Map<String, Map<String, String>> expect) {
+    List<String> input = expect.keySet().stream()
+        .map(v -> "{\"attributeMap\":{\"user_agent\":\"" + v + "\"},\"payload\":\"\"}")
+        .collect(Collectors.toList());
+    List<String> expected = expect.values().stream()
+        .map(v -> "{\"attributeMap\":{" + String.join(",",
+            v.entrySet().stream().map(e -> "\"" + e.getKey() + "\":\"" + e.getValue() + "\"")
+                .collect(Collectors.toList()))
+            + "},\"payload\":\"\"}")
+        .collect(Collectors.toList());
 
     final PCollection<String> output = pipeline //
         .apply(Create.of(input)) //
@@ -47,4 +103,67 @@ public class ParseUserAgentTest {
     pipeline.run();
   }
 
+  private void test(List<String> keys, List<Map<String, String>> values) {
+    ImmutableMap.Builder<String, Map<String, String>> builder = new ImmutableMap.Builder<>();
+    for (int i = 0; i < keys.size(); i++) {
+      builder = builder.put(keys.get(i), values.get(i));
+    }
+    test(builder.build());
+  }
+
+  @Test
+  public void testBrowserSingleMatch() {
+    test(browserSingleMatch);
+  }
+
+  @Test
+  public void testBrowserBestMatchFirst() {
+    test(browserBestMatch.stream().map(v -> String.join("", v)).collect(Collectors.toList()),
+        browserSingleMatch.entrySet().stream().map(e -> {
+          switch (e.getKey()) {
+            case "Safari":
+            case "Opera":
+            case "Opera Mini":
+            case "Opera Mobi":
+              return ImmutableMap.of(BROWSER, e.getKey(), VERSION, "6");
+            default:
+              return e.getValue();
+          }
+        }).collect(Collectors.toList()));
+  }
+
+  @Test
+  public void testBrowserBestMatchLast() {
+    test(browserBestMatch.stream().map(l -> String.join("", Lists.reverse(l)))
+        .collect(Collectors.toList()), browserSingleMatch.entrySet().stream().map(e -> {
+          String version = null;
+          switch (e.getKey()) {
+            case "Safari":
+              return ImmutableMap.of(BROWSER, e.getKey(), VERSION, "5");
+            case "Opera":
+            case "Opera Mini":
+            case "Opera Mobi":
+              return ImmutableMap.of(BROWSER, e.getKey(), VERSION, "7");
+            default:
+              return e.getValue();
+          }
+        }).collect(Collectors.toList()));
+  }
+
+  @Test
+  public void testOsSingleMatch() {
+    test(osSingleMatch);
+  }
+
+  @Test
+  public void testOsBestMatchFirst() {
+    test(osBestMatch.stream().map(l -> String.join("", l)).collect(Collectors.toList()),
+        new ArrayList<>(osSingleMatch.values()));
+  }
+
+  @Test
+  public void testOsBestMatchLast() {
+    test(osBestMatch.stream().map(l -> String.join("", Lists.reverse(l)))
+        .collect(Collectors.toList()), new ArrayList<>(osSingleMatch.values()));
+  }
 }
