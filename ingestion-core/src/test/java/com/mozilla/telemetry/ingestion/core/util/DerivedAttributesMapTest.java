f5709b6409a85c47d68488ec689da0c19bbd40c0
Daniel Thorn <dthorn@mozilla.com>
2019-06-19 12:19:56 -0700

    Provide derived attributes in batchKeyTemplate

--- ingestion-core/src/test/java/com/mozilla/telemetry/ingestion/core/util/DerivedAttributesMapTest.java
+++ ingestion-core/src/test/java/com/mozilla/telemetry/ingestion/core/util/DerivedAttributesMapTest.java
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.mozilla.telemetry.util;
+package com.mozilla.telemetry.ingestion.core.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
