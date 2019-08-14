3cae324fa7336830c192338d6aedd795b2b05d54
Daniel Thorn <dthorn@mozilla.com>
2019-08-14 14:11:00 -0700

    Implement decoded format for ingestion-sink

--- ingestion-sink/src/test/java/com/mozilla/telemetry/ingestion/transform/PubsubMessageToTableRowTest.java
+++ ingestion-sink/src/test/java/com/mozilla/telemetry/ingestion/transform/PubsubMessageToTableRowTest.java
@@ -15,12 +15,12 @@ import org.junit.Test;
 
 public class PubsubMessageToTableRowTest {
 
-  private static final PubsubMessageToTableRow TRANSFORM = new PubsubMessageToTableRow(
+  private static final PubsubMessageToTableRow RAW_TRANSFORM = new PubsubMessageToTableRow(
       "${document_namespace}_raw.${document_type}_v${document_version}", TableRowFormat.raw);
 
   @Test
-  public void canTransformSingleMessage() {
-    final TableRow actual = TRANSFORM
+  public void canFormatAsRaw() {
+    final TableRow actual = RAW_TRANSFORM
         .apply(PubsubMessage.newBuilder().setData(ByteString.copyFrom("test".getBytes()))
             .putAttributes("document_id", "id").putAttributes("document_namespace", "telemetry")
             .putAttributes("document_type", "main").putAttributes("document_version", "4").build());
@@ -34,8 +34,8 @@ public class PubsubMessageToTableRowTest {
   }
 
   @Test
-  public void canHandleEmptyValues() {
-    final TableRow actual = TRANSFORM
+  public void canFormatAsRawWithEmptyValues() {
+    final TableRow actual = RAW_TRANSFORM
         .apply(PubsubMessage.newBuilder().putAttributes("document_namespace", "")
             .putAttributes("document_type", "").putAttributes("document_version", "").build());
 
@@ -50,7 +50,7 @@ public class PubsubMessageToTableRowTest {
 
   @Test(expected = IllegalArgumentException.class)
   public void failsOnMissingAttributes() {
-    TRANSFORM.apply(PubsubMessage.newBuilder().build());
+    RAW_TRANSFORM.apply(PubsubMessage.newBuilder().build());
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -60,12 +60,155 @@ public class PubsubMessageToTableRowTest {
 
   @Test(expected = IllegalArgumentException.class)
   public void failsOnUnimplementedFormat() {
-    new PubsubMessageToTableRow("dataset.table", TableRowFormat.decoded)
+    new PubsubMessageToTableRow("dataset.table", TableRowFormat.payload)
         .apply(PubsubMessage.newBuilder().build());
   }
 
   @Test(expected = NullPointerException.class)
   public void failsOnNullMessage() {
-    TRANSFORM.apply(null);
+    RAW_TRANSFORM.apply(null);
+  }
+
+  private static final PubsubMessageToTableRow DECODED_TRANSFORM = new PubsubMessageToTableRow(
+      "${document_namespace}_decoded.${document_type}_v${document_version}",
+      TableRowFormat.decoded);
+
+  @Test
+  public void canFormatTelemetryAsDecoded() {
+    final TableRow actual = DECODED_TRANSFORM.apply(PubsubMessage.newBuilder()
+        .setData(ByteString.copyFrom("test".getBytes())).putAttributes("geo_city", "geo_city")
+        .putAttributes("geo_country", "geo_country")
+        .putAttributes("geo_subdivision1", "geo_subdivision1")
+        .putAttributes("geo_subdivision2", "geo_subdivision2")
+        .putAttributes("user_agent_browser", "user_agent_browser")
+        .putAttributes("user_agent_os", "user_agent_os")
+        .putAttributes("user_agent_version", "user_agent_version").putAttributes("date", "date")
+        .putAttributes("dnt", "dnt").putAttributes("x_debug_id", "x_debug_id")
+        .putAttributes("x_pingsender_version", "x_pingsender_version").putAttributes("uri", "uri")
+        .putAttributes("app_build_id", "app_build_id").putAttributes("app_name", "app_name")
+        .putAttributes("app_update_channel", "app_update_channel")
+        .putAttributes("app_version", "app_version")
+        .putAttributes("document_namespace", "telemetry")
+        .putAttributes("document_type", "document_type")
+        .putAttributes("document_version", "document_version")
+        .putAttributes("submission_timestamp", "submission_timestamp")
+        .putAttributes("document_id", "document_id")
+        .putAttributes("normalized_app_name", "normalized_app_name")
+        .putAttributes("normalized_channel", "normalized_channel")
+        .putAttributes("normalized_country_code", "normalized_country_code")
+        .putAttributes("normalized_os", "normalized_os")
+        .putAttributes("normalized_os_version", "normalized_os_version")
+        .putAttributes("sample_id", "42").putAttributes("client_id", "client_id").build());
+
+    assertEquals("telemetry_decoded", actual.tableId.getDataset());
+    assertEquals("document_type_vdocument_version", actual.tableId.getTable());
+    assertEquals(916, actual.byteSize);
+    assertEquals("test", new String((byte[]) actual.content.remove("payload")));
+
+    assertEquals(
+        ImmutableMap.builder()
+            .put("metadata", ImmutableMap.builder()
+                .put("geo",
+                    ImmutableMap.builder().put("country", "geo_country").put("city", "geo_city")
+                        .put("subdivision2", "geo_subdivision2")
+                        .put("subdivision1", "geo_subdivision1").build())
+                .put("user_agent",
+                    ImmutableMap.builder().put("browser", "user_agent_browser")
+                        .put("os", "user_agent_os").put("version", "user_agent_version").build())
+                .put("header",
+                    ImmutableMap.builder().put("date", "date").put("dnt", "dnt")
+                        .put("x_debug_id", "x_debug_id")
+                        .put("x_pingsender_version", "x_pingsender_version").build())
+                .put("uri",
+                    ImmutableMap.builder().put("uri", "uri").put("app_build_id", "app_build_id")
+                        .put("app_name", "app_name").put("app_update_channel", "app_update_channel")
+                        .put("app_version", "app_version").build())
+                .put("document_namespace", "telemetry").put("document_type", "document_type")
+                .put("document_version", "document_version").build())
+            .put("submission_timestamp", "submission_timestamp").put("document_id", "document_id")
+            .put("normalized_app_name", "normalized_app_name")
+            .put("normalized_channel", "normalized_channel")
+            .put("normalized_country_code", "normalized_country_code")
+            .put("normalized_os", "normalized_os")
+            .put("normalized_os_version", "normalized_os_version").put("sample_id", 42)
+            .put("client_id", "client_id").build(),
+        actual.content);
+  }
+
+  @Test
+  public void canFormatNonTelemetryAsDecoded() {
+    final TableRow actual = DECODED_TRANSFORM.apply(PubsubMessage.newBuilder()
+        .setData(ByteString.copyFrom("test".getBytes())).putAttributes("geo_city", "geo_city")
+        .putAttributes("geo_country", "geo_country")
+        .putAttributes("geo_subdivision1", "geo_subdivision1")
+        .putAttributes("geo_subdivision2", "geo_subdivision2")
+        .putAttributes("user_agent_browser", "user_agent_browser")
+        .putAttributes("user_agent_os", "user_agent_os")
+        .putAttributes("user_agent_version", "user_agent_version").putAttributes("date", "date")
+        .putAttributes("dnt", "dnt").putAttributes("x_debug_id", "x_debug_id")
+        .putAttributes("x_pingsender_version", "x_pingsender_version").putAttributes("uri", "uri")
+        .putAttributes("app_build_id", "app_build_id").putAttributes("app_name", "app_name")
+        .putAttributes("app_update_channel", "app_update_channel")
+        .putAttributes("app_version", "app_version")
+        .putAttributes("document_namespace", "document_namespace")
+        .putAttributes("document_type", "document_type")
+        .putAttributes("document_version", "document_version")
+        .putAttributes("submission_timestamp", "submission_timestamp")
+        .putAttributes("document_id", "document_id")
+        .putAttributes("normalized_app_name", "normalized_app_name")
+        .putAttributes("normalized_channel", "normalized_channel")
+        .putAttributes("normalized_country_code", "normalized_country_code")
+        .putAttributes("normalized_os", "normalized_os")
+        .putAttributes("normalized_os_version", "normalized_os_version")
+        .putAttributes("sample_id", "42").putAttributes("client_id", "client_id").build());
+
+    assertEquals("document_namespace_decoded", actual.tableId.getDataset());
+    assertEquals("document_type_vdocument_version", actual.tableId.getTable());
+    assertEquals(925, actual.byteSize);
+    assertEquals("test", new String((byte[]) actual.content.remove("payload")));
+
+    assertEquals(
+        ImmutableMap.builder()
+            .put("metadata", ImmutableMap.builder()
+                .put("geo",
+                    ImmutableMap.builder().put("country", "geo_country").put("city", "geo_city")
+                        .put("subdivision2", "geo_subdivision2")
+                        .put("subdivision1", "geo_subdivision1").build())
+                .put("user_agent",
+                    ImmutableMap.builder().put("browser", "user_agent_browser")
+                        .put("os", "user_agent_os").put("version", "user_agent_version").build())
+                .put("header",
+                    ImmutableMap.builder().put("date", "date").put("dnt", "dnt")
+                        .put("x_debug_id", "x_debug_id")
+                        .put("x_pingsender_version", "x_pingsender_version").build())
+                .put("document_namespace", "document_namespace")
+                .put("document_type", "document_type").put("document_version", "document_version")
+                .build())
+            .put("submission_timestamp", "submission_timestamp").put("document_id", "document_id")
+            .put("normalized_app_name", "normalized_app_name")
+            .put("normalized_channel", "normalized_channel")
+            .put("normalized_country_code", "normalized_country_code")
+            .put("normalized_os", "normalized_os")
+            .put("normalized_os_version", "normalized_os_version").put("sample_id", 42)
+            .put("client_id", "client_id").build(),
+        actual.content);
+  }
+
+  @Test
+  public void canFormatAsDecodedWithEmptyValues() {
+    final TableRow actual = DECODED_TRANSFORM.apply(PubsubMessage.newBuilder()
+        .putAttributes("document_namespace", "").putAttributes("document_type", "")
+        .putAttributes("document_version", "").putAttributes("sample_id", "").build());
+
+    assertEquals("_decoded", actual.tableId.getDataset());
+    assertEquals("_v", actual.tableId.getTable());
+    assertEquals(80, actual.byteSize);
+    assertEquals("", new String((byte[]) actual.content.remove("payload")));
+    assertEquals(
+        ImmutableMap.of("metadata",
+            ImmutableMap.builder().put("document_namespace", "").put("document_type", "")
+                .put("document_version", "").put("geo", ImmutableMap.of())
+                .put("header", ImmutableMap.of()).put("user_agent", ImmutableMap.of()).build()),
+        actual.content);
   }
 }
