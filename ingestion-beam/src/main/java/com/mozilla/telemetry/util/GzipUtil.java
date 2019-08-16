03695eeb2ef3e266547101412dda5dc1b7587174
Jeff Klukas <jeff@klukas.net>
2019-08-16 14:49:34 -0400

    Delay uncompressing payloads until writing to GCS

--- ingestion-beam/src/main/java/com/mozilla/telemetry/util/GzipUtil.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/util/GzipUtil.java
@@ -13,11 +13,9 @@ import org.apache.commons.io.IOUtils;
 public class GzipUtil {
 
   public static byte[] maybeDecompress(byte[] bytes) {
-    try (
-        ByteArrayInputStream payloadStream = new ByteArrayInputStream(bytes);
+    try (ByteArrayInputStream payloadStream = new ByteArrayInputStream(bytes);
         GZIPInputStream gzipStream = new GZIPInputStream(payloadStream);
-        ByteArrayOutputStream decompressedStream = new ByteArrayOutputStream();
-        ) {
+        ByteArrayOutputStream decompressedStream = new ByteArrayOutputStream();) {
       IOUtils.copy(gzipStream, decompressedStream);
       return decompressedStream.toByteArray();
     } catch (IOException e) {
