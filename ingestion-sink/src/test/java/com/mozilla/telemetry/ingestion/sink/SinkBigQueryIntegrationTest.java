0664c46643b7de87548466c78d3d26b011163cf2
Daniel Thorn <dthorn@mozilla.com>
2020-01-08 09:26:11 -0800

    Add a BoundedSink util for sink integration tests (#1063)
    And move GCP resources into JUnit rules so they are cleaned up even when
    tests fail

--- ingestion-sink/src/test/java/com/mozilla/telemetry/ingestion/sink/SinkBigQueryIntegrationTest.java
+++ ingestion-sink/src/test/java/com/mozilla/telemetry/ingestion/sink/SinkBigQueryIntegrationTest.java
@@ -2,9 +2,7 @@ package com.mozilla.telemetry.ingestion.sink;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.Clustering;
-import com.google.cloud.bigquery.DatasetInfo;
 import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.LegacySQLTypeName;
 import com.google.cloud.bigquery.QueryJobConfiguration;
@@ -13,12 +11,13 @@ import com.google.cloud.bigquery.StandardTableDefinition;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
 import com.google.cloud.bigquery.TimePartitioning;
-import com.google.cloud.bigquery.testing.RemoteBigQueryHelper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
-import com.mozilla.telemetry.ingestion.sink.util.TestWithSinglePubsubTopic;
+import com.mozilla.telemetry.ingestion.sink.util.BigQueryDataset;
+import com.mozilla.telemetry.ingestion.sink.util.BoundedSink;
+import com.mozilla.telemetry.ingestion.sink.util.SinglePubsubTopic;
 import java.nio.charset.StandardCharsets;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -28,45 +27,21 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
-import org.junit.rules.Timeout;
 
-public class SinkBigQueryIntegrationTest extends TestWithSinglePubsubTopic {
+public class SinkBigQueryIntegrationTest {
 
-  private BigQuery bigquery;
-  private String project;
-  private String dataset;
+  @Rule
+  public final SinglePubsubTopic pubsub = new SinglePubsubTopic();
 
-  /** Find credentials in the environment and create a dataset in BigQuery. */
-  @Before
-  public void createBigQueryDataset() {
-    RemoteBigQueryHelper bqHelper = RemoteBigQueryHelper.create();
-    bigquery = bqHelper.getOptions().getService();
-    project = bqHelper.getOptions().getProjectId();
-    dataset = RemoteBigQueryHelper.generateDatasetName();
-    bigquery.create(DatasetInfo.newBuilder(dataset).build());
-  }
-
-  /** Remove all resources we created in BigQuery. */
-  @After
-  public void deleteBigQueryDataset() {
-    RemoteBigQueryHelper.forceDelete(bigquery, dataset);
-  }
+  @Rule
+  public final BigQueryDataset bq = new BigQueryDataset();
 
   @Rule
   public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
-
-  @Rule
-  public final Timeout timeout = new Timeout(30, TimeUnit.SECONDS);
 
   private static final TimePartitioning TIME_PARTITIONING = TimePartitioning
       .newBuilder(TimePartitioning.Type.DAY).setField("submission_timestamp").build();
@@ -89,8 +64,8 @@ public class SinkBigQueryIntegrationTest extends TestWithSinglePubsubTopic {
     List<String> versions = ImmutableList.of("1", "2");
     versions.forEach(version -> {
       String table = "test_v" + version;
-      TableId tableId = TableId.of(dataset, table);
-      bigquery.create(TableInfo.newBuilder(tableId, tableDef).build());
+      TableId tableId = TableId.of(bq.dataset, table);
+      bq.bigquery.create(TableInfo.newBuilder(tableId, tableDef).build());
       PubsubMessage.Builder message = PubsubMessage.newBuilder()
           .setData(ByteString.copyFrom("test".getBytes(StandardCharsets.UTF_8)))
           .putAttributes("document_type", "test").putAttributes("document_version", version)
@@ -98,43 +73,32 @@ public class SinkBigQueryIntegrationTest extends TestWithSinglePubsubTopic {
       if (documentIds.containsKey(version)) {
         message.putAttributes("document_id", documentIds.get(version));
       }
-      publish(message.build());
+      pubsub.publish(message.build());
     });
 
-    environmentVariables.set("INPUT_SUBSCRIPTION", getSubscription());
+    environmentVariables.set("INPUT_SUBSCRIPTION", pubsub.getSubscription());
     environmentVariables.set("OUTPUT_TABLE",
-        project + "." + dataset + ".${document_type}_v${document_version}");
+        bq.project + "." + bq.dataset + ".${document_type}_v${document_version}");
 
-    CompletableFuture<Void> main = CompletableFuture.runAsync(() -> Sink.main(new String[] {}));
+    BoundedSink.run(versions.size(), 30);
 
     String query = "SELECT * REPLACE (FORMAT_TIMESTAMP('%FT%R:%E*SZ', submission_timestamp) AS "
         + "submission_timestamp), _TABLE_SUFFIX AS table FROM "
-        + String.format("`%s.%s.*`", project, dataset);
-    AtomicReference<List<List<String>>> actual = new AtomicReference<>();
-    do {
-      actual.set(new LinkedList<>());
-      bigquery.query(QueryJobConfiguration.of(query)).iterateAll()
-          .forEach(row -> actual.get().add(row.stream().map(fv -> {
-            try {
-              return fv.getStringValue();
-            } catch (NullPointerException e) {
-              return "";
-            }
-          }).collect(Collectors.toList())));
-    } while (actual.get().size() < versions.size());
-    assertEquals(versions.size(), actual.get().size());
-
-    main.cancel(true);
-    try {
-      main.join();
-    } catch (CancellationException ignore) {
-      // ignore
-    }
+        + String.format("`%s.%s.*`", bq.project, bq.dataset);
+    List<List<String>> actual = new LinkedList<>();
+    bq.bigquery.query(QueryJobConfiguration.of(query)).iterateAll()
+        .forEach(row -> actual.add(row.stream().map(fv -> {
+          try {
+            return fv.getStringValue();
+          } catch (NullPointerException e) {
+            return "";
+          }
+        }).collect(Collectors.toList())));
 
     List<List<String>> expected = ImmutableList.of(
         ImmutableList.of(documentIds.get("1"), "test", "1", submissionTimestamp, "test_v1"),
         ImmutableList.of("", "test", "2", submissionTimestamp, "test_v2"));
-    actual.get().sort(Comparator.comparing(row -> row.get(4)));
-    assertEquals(expected, actual.get());
+    actual.sort(Comparator.comparing(row -> row.get(4)));
+    assertEquals(expected, actual);
   }
 }
