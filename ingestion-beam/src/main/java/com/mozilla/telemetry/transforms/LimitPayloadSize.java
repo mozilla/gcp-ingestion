999382ce654dab79224179790b42820f70de0eef
Anna Scholtz <anna@scholtzan.net>
2020-01-30 17:55:40 +0100

    Factory method for creating MapElements instance in LimitPayloadSize

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/LimitPayloadSize.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/LimitPayloadSize.java
@@ -4,14 +4,11 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.metrics.Counter;
 import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.MapElements.MapWithFailures;
 import org.apache.beam.sdk.transforms.WithFailures.ExceptionElement;
-import org.apache.beam.sdk.transforms.WithFailures.Result;
-import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptor;
 
-public class LimitPayloadSize extends
-    PTransform<PCollection<PubsubMessage>, Result<PCollection<PubsubMessage>, PubsubMessage>> {
+public class LimitPayloadSize {
 
   /** Exception to throw for messages whose payloads exceed the configured size limit. */
   public static class PayloadTooLargeException extends RuntimeException {
@@ -22,46 +19,35 @@ public class LimitPayloadSize extends
   }
 
   /** Return a transform that will send messages to error output if payload exceeds maxBytes. */
-  public static LimitPayloadSize toBytes(int maxBytes) {
-    return new LimitPayloadSize(maxBytes);
+  public static MapWithFailures<PubsubMessage, PubsubMessage, PubsubMessage> toBytes(int maxBytes) {
+    return LimitPayloadSize.of(maxBytes);
   }
 
   /** Return a transform that will send messages to error output if payload exceeds mb megabytes. */
-  public static LimitPayloadSize toMB(int mb) {
-    return new LimitPayloadSize(mb * 1024 * 1024);
+  public static MapWithFailures<PubsubMessage, PubsubMessage, PubsubMessage> toMB(int mb) {
+    return LimitPayloadSize.of(mb * 1024 * 1024);
   }
 
-  @Override
-  public Result<PCollection<PubsubMessage>, PubsubMessage> expand(
-      PCollection<PubsubMessage> elements) {
-    return elements.apply(
-        MapElements.into(TypeDescriptor.of(PubsubMessage.class)).via((PubsubMessage message) -> {
-          message = PubsubConstraints.ensureNonNull(message);
-          int numBytes = message.getPayload().length;
-          if (numBytes > maxBytes) {
-            countPayloadTooLarge.inc();
-            throw new PayloadTooLargeException("Message payload is " + numBytes
-                + "bytes, larger than the" + " configured limit of " + maxBytes);
+  /** Factory method to create mapper instance. */
+  public static MapWithFailures<PubsubMessage, PubsubMessage, PubsubMessage> of(int maxBytes) {
+    final Counter countPayloadTooLarge = Metrics.counter(LimitPayloadSize.class,
+        "payload_too_large");
+    return MapElements.into(TypeDescriptor.of(PubsubMessage.class)).via((PubsubMessage msg) -> {
+      msg = PubsubConstraints.ensureNonNull(msg);
+      int numBytes = msg.getPayload().length;
+      if (numBytes > maxBytes) {
+        countPayloadTooLarge.inc();
+        throw new PayloadTooLargeException("Message payload is " + numBytes
+            + "bytes, larger than the" + " configured limit of " + maxBytes);
+      }
+      return msg;
+    }).exceptionsInto(TypeDescriptor.of(PubsubMessage.class))
+        .exceptionsVia((ExceptionElement<PubsubMessage> ee) -> {
+          try {
+            throw ee.exception();
+          } catch (PayloadTooLargeException e) {
+            return FailureMessage.of("LimitPayloadSize", ee.element(), ee.exception());
           }
-          return message;
-        }).exceptionsInto(TypeDescriptor.of(PubsubMessage.class))
-            .exceptionsVia((ExceptionElement<PubsubMessage> ee) -> {
-              try {
-                throw ee.exception();
-              } catch (PayloadTooLargeException e) {
-                return FailureMessage.of(this, ee.element(), ee.exception());
-              }
-            }));
-  }
-
-  ////////
-
-  private final int maxBytes;
-
-  private final Counter countPayloadTooLarge = Metrics.counter(LimitPayloadSize.class,
-      "payload_too_large");
-
-  private LimitPayloadSize(int maxBytes) {
-    this.maxBytes = maxBytes;
+        });
   }
 }
