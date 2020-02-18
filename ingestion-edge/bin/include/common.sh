351b21d4bdc10acbc0f38887f430fa5e7408aba7
Daniel Thorn <dthorn@mozilla.com>
2020-02-18 11:09:05 -0800

    Fix pytest-docstyle config

--- ingestion-edge/bin/include/common.sh
+++ ingestion-edge/bin/include/common.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 cd "$(dirname "$0")"/..
 
-LINT_ARGS=( --black --docstyle --flake8 --mypy-ignore-missing-imports )
+LINT_ARGS=( --black --pydocstyle --flake8 --mypy-ignore-missing-imports )
 
 VENV_DIR="$PWD/venv/$(uname)"
 VENV_ACTIVATE="$VENV_DIR/bin/activate"
