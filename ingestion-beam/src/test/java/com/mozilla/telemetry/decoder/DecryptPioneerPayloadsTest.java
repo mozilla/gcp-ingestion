1f12cc293c9396a3f0c8b0224231e74ba0a59ffc
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-10 16:34:46 -0700

    Implement decryption of a single document

--- ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/DecryptPioneerPayloadsTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/DecryptPioneerPayloadsTest.java
@@ -1,10 +1,13 @@
 package com.mozilla.telemetry.decoder;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
 import com.mozilla.telemetry.options.InputFileFormat;
 import com.mozilla.telemetry.options.OutputFileFormat;
+import com.mozilla.telemetry.transforms.DecompressPayload;
+import com.mozilla.telemetry.util.Json;
 import com.mozilla.telemetry.util.TestWithDeterministicJson;
 import java.util.Arrays;
 import java.util.List;
@@ -14,9 +17,10 @@ import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.WithFailures.Result;
+import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.TypeDescriptors;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -28,11 +32,33 @@ public class DecryptPioneerPayloadsTest extends TestWithDeterministicJson {
   private List<String> readTestFiles(List<String> filenames) {
     return Arrays.asList(filenames.stream().map(Resources::getResource).map(url -> {
       try {
-        return Resources.toString(url, Charsets.UTF_8);
+        // format for comparison in testing
+        String data = Resources.toString(url, Charsets.UTF_8);
+        ObjectNode json = Json.readObjectNode(data);
+        return json.toString();
       } catch (Exception e) {
         return null;
       }
     }).toArray(String[]::new));
+  }
+
+  final private static class ReformatJson
+      extends PTransform<PCollection<String>, PCollection<String>> {
+
+    public static ReformatJson of() {
+      return new ReformatJson();
+    }
+
+    @Override
+    public PCollection<String> expand(PCollection<String> input) {
+      return input.apply(MapElements.into(TypeDescriptors.strings()).via(s -> {
+        try {
+          return Json.readObjectNode(s).toString();
+        } catch (Exception e) {
+          return null;
+        }
+      }));
+    }
   }
 
   @Test
@@ -40,23 +66,21 @@ public class DecryptPioneerPayloadsTest extends TestWithDeterministicJson {
     // minimal test for throughput of a single document
     ValueProvider<String> keysLocation = pipeline
         .newProvider(Resources.getResource("pioneer/id_rsa_0.private.json").getPath());
-    final List<String> input = readTestFiles(Arrays.asList("pioneer/sample.cleartext.json"));
+    final List<String> input = readTestFiles(Arrays.asList("pioneer/sample.ciphertext.json"));
 
-    Result<PCollection<PubsubMessage>, PubsubMessage> output = pipeline
-        .apply(Create.of(input)).apply(
-            InputFileFormat.text.decode())
+    PCollection<String> output = pipeline.apply(Create.of(input))
+        .apply(InputFileFormat.text.decode())
         .apply("AddAttributes",
             MapElements.into(TypeDescriptor.of(PubsubMessage.class))
-                .via(
-                    element -> new PubsubMessage(element.getPayload(),
-                        ImmutableMap.of("document_namespace", "pioneer", "document_type", "test",
-                            "document_version", "1"))))
-        .apply(DecryptPioneerPayloads.of(keysLocation));
+                .via(element -> new PubsubMessage(element.getPayload(),
+                    ImmutableMap.of("document_namespace", "pioneer", "document_type", "test",
+                        "document_version", "1"))))
+        .apply(DecryptPioneerPayloads.of(keysLocation)).output()
+        .apply(DecompressPayload.enabled(pipeline.newProvider(true)))
+        .apply(OutputFileFormat.text.encode()).apply(ReformatJson.of());
 
-    final List<String> expectedMain = input;
-    final PCollection<String> main = output.output().apply("encodeTextMain",
-        OutputFileFormat.text.encode());
-    PAssert.that(main).containsInAnyOrder(expectedMain);
+    final List<String> expectedMain = readTestFiles(Arrays.asList("pioneer/sample.cleartext.json"));
+    PAssert.that(output).containsInAnyOrder(expectedMain);
 
     pipeline.run();
   }
