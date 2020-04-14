201631e3585fa3c3300613225856ee6195d8a86e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-14 15:11:22 -0700

    Add a few basic tests for KeyStore

--- ingestion-beam/src/test/java/com/mozilla/telemetry/util/KeyStoreTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/util/KeyStoreTest.java
@@ -1,5 +1,8 @@
 package com.mozilla.telemetry.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import com.google.common.io.Resources;
 import org.everit.json.schema.ValidationException;
 import org.junit.Test;
@@ -16,10 +19,17 @@ public class KeyStoreTest {
 
   @Test
   public void testNumKeys() {
+    String metadataLocation = Resources.getResource("pioneer/metadata-simple.json").getPath();
+    KeyStore store = KeyStore.of(metadataLocation);
+    assertEquals(1, store.numLoadedKeys());
   }
 
   @Test
   public void testGetKey() {
+    String metadataLocation = Resources.getResource("pioneer/metadata-simple.json").getPath();
+    KeyStore store = KeyStore.of(metadataLocation);
+    assertNotEquals(null, store.getKey("pioneer"));
+    assertEquals(null, store.getKey("invalid_document_name"));
   }
 
 }
