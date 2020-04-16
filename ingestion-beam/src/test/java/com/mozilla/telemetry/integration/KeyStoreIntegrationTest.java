c91236c2fce0105bba684abe942191c9b68ba8bb
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-16 09:42:28 -0700

    wip: add test for KMS

--- ingestion-beam/src/test/java/com/mozilla/telemetry/integration/KeyStoreIntegrationTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/integration/KeyStoreIntegrationTest.java
@@ -1,14 +1,25 @@
 package com.mozilla.telemetry.integration;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.api.gax.rpc.NotFoundException;
+import com.google.cloud.kms.v1.CryptoKey;
+import com.google.cloud.kms.v1.CryptoKeyName;
+import com.google.cloud.kms.v1.CryptoKeyVersion.CryptoKeyVersionAlgorithm;
+import com.google.cloud.kms.v1.CryptoKeyVersionTemplate;
+import com.google.cloud.kms.v1.DecryptResponse;
+import com.google.cloud.kms.v1.EncryptResponse;
 import com.google.cloud.kms.v1.KeyManagementServiceClient;
 import com.google.cloud.kms.v1.KeyRing;
+import com.google.cloud.kms.v1.KeyRingName;
 import com.google.cloud.kms.v1.LocationName;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.testing.RemoteStorageHelper;
+import com.google.protobuf.ByteString;
 import com.mozilla.telemetry.util.TestWithDeterministicJson;
 import java.io.IOException;
-import java.util.UUID;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
@@ -49,23 +60,44 @@ public class KeyStoreIntegrationTest extends TestWithDeterministicJson {
     RemoteStorageHelper.forceDelete(storage, bucket, 5, TimeUnit.SECONDS);
   }
 
-  private String saltName(String name) {
-    String salt = UUID.randomUUID().toString().substring(0, 8);
-    return String.format("%s_%s", name, salt);
-  }
-
   @Test
   public void testKeyRing() throws IOException {
     try (KeyManagementServiceClient client = KeyManagementServiceClient.create()) {
       // Generate the keyring
-      LocationName parent = LocationName.of(projectId, "global");
-      String keyRingId = saltName("test");
-      KeyRing keyRing = KeyRing.newBuilder().build();
-      client.createKeyRing(parent, keyRingId, keyRing);
+      // KeyRings and Keys cannot be deleted, so reuse resources
+      String keyRingId = "test-ingestion-beam-integration";
+      KeyRingName keyRingName = KeyRingName.of(projectId, "global", keyRingId);
 
-      // Generate the key
+      try {
+        client.getKeyRing(keyRingName);
+      } catch (NotFoundException e) {
+        LocationName parent = LocationName.of(projectId, "global");
+        KeyRing request = KeyRing.newBuilder().build();
+        client.createKeyRing(parent, keyRingId, request);
+      }
+
+      // Get or generate the key
+      String keyName = "test";
+      String cryptoKeyName = CryptoKeyName.of(projectId, "global", keyRingId, keyName).toString();
+      try {
+        client.getCryptoKey(cryptoKeyName);
+      } catch (NotFoundException e) {
+        CryptoKey request = CryptoKey.newBuilder()
+            .setPurpose(CryptoKey.CryptoKeyPurpose.ASYMMETRIC_DECRYPT)
+            .setVersionTemplate(CryptoKeyVersionTemplate.newBuilder()
+                .setAlgorithm(CryptoKeyVersionAlgorithm.RSA_DECRYPT_OAEP_2048_SHA256))
+            .build();
+        client.createCryptoKey(keyRingName, keyName, request);
+      }
+
       // Encrypt the data
+      // TODO: this fails because KMS only supports decryption with asymmetric keys
+      ByteString plaintext = ByteString.copyFrom("hello world", Charset.defaultCharset());
+      EncryptResponse encrypted = client.encrypt(cryptoKeyName, plaintext);
+
       // decrypt the data
+      DecryptResponse decrypted = client.decrypt(cryptoKeyName, encrypted.getCiphertext());
+      assertEquals(plaintext, decrypted.getPlaintext());
     }
   }
 
