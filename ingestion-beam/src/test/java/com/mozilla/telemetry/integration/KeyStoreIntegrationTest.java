7d5101c43d4b2e2d16d91770546e936f6177c3be
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-17 14:21:42 -0700

    Add passing test for reading keys from cloud storage

--- ingestion-beam/src/test/java/com/mozilla/telemetry/integration/KeyStoreIntegrationTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/integration/KeyStoreIntegrationTest.java
@@ -1,7 +1,12 @@
 package com.mozilla.telemetry.integration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.api.gax.rpc.NotFoundException;
 import com.google.cloud.kms.v1.CryptoKey;
 import com.google.cloud.kms.v1.CryptoKeyName;
@@ -16,12 +21,24 @@ import com.google.cloud.kms.v1.PublicKey;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.testing.RemoteStorageHelper;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Resources;
 import com.google.protobuf.ByteString;
+import com.mozilla.telemetry.util.Json;
+import com.mozilla.telemetry.util.KeyStore;
 import com.mozilla.telemetry.util.TestWithDeterministicJson;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.concurrent.TimeUnit;
+import org.apache.beam.sdk.io.FileSystems;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.util.MimeTypes;
 import org.apache.commons.io.IOUtils;
 import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.bouncycastle.crypto.digests.SHA256Digest;
@@ -145,4 +162,59 @@ public class KeyStoreIntegrationTest extends TestWithDeterministicJson {
     }
   }
 
+  /**
+   * Write to cloud storage using the FileSystems API. See https://stackoverflow.com/a/50050583.
+   */
+  private void writeToCloudStorage(String path, byte[] data) throws IOException {
+    ResourceId resourceId = FileSystems.matchNewResource(path, false);
+    try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        ReadableByteChannel readerChannel = Channels.newChannel(inputStream);
+        WritableByteChannel writerChannel = FileSystems.create(resourceId, MimeTypes.TEXT)) {
+      ByteStreams.copy(readerChannel, writerChannel);
+    }
+  }
+
+  @Test
+  public void testKeyStoreReadsPlaintextPrivateKeyFromCloudStorage() throws IOException {
+    // enable gs support
+    FileSystems.setDefaultPipelineOptions(PipelineOptionsFactory.create());
+
+    byte[] data = Resources.toByteArray(Resources.getResource("pioneer/metadata-integration.json"));
+    ArrayNode nodes = Json.readArrayNode(data);
+    for (JsonNode node : nodes) {
+      // replace dummy values with values related to integration testing
+      String kmsResourceId = node.get("kms_resource_id").textValue().replace("DUMMY_PROJECT_ID",
+          projectId);
+
+      // The path may be on the local filesystem or in cloud storage by
+      // referencing a variable that is replaced.
+      String privateKeyUri = node.get("private_key_uri").textValue().replace("DUMMY_BUCKET", bucket)
+          .replace("DUMMY_TEMP_FOLDER", tempFolder.getRoot().toString());
+      ((ObjectNode) node).put("kms_resource_id", kmsResourceId);
+      ((ObjectNode) node).put("private_key_uri", privateKeyUri);
+
+      // upload the resource to the appropriate location
+      String keyId = node.get("document_namespace").textValue();
+      byte[] key = Resources
+          .toByteArray(Resources.getResource(String.format("pioneer/%s.private.json", keyId)));
+      writeToCloudStorage(privateKeyUri, key);
+    }
+    assertFalse(
+        nodes.asText().contains("DUMMY_PROJECT_ID") && nodes.asText().contains("DUMMY_BUCKET"));
+
+    String keyStoreMetadata = String.format("gs://%s/metadata.json", bucket);
+    writeToCloudStorage(keyStoreMetadata, nodes.toString().getBytes());
+
+    KeyStore store = KeyStore.of(keyStoreMetadata);
+    assertNotEquals(null, store.getKey("study_foo"));
+    assertNotEquals(null, store.getKey("study_bar"));
+  }
+
+  @Test
+  public void testKeyStoreEncryptedKeysCanDecryptPayload() {
+    assert (false);
+    // upload modified JSON to cloud storage
+    // upload encrypted keys
+    // decrypt one of the sample messages
+  }
 }
