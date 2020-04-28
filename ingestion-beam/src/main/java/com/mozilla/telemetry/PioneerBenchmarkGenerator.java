3c3bf2a6d8c9a9f682fb390d0c71752450cdd6a8
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-27 17:30:31 -0700

    Avoid using beam due to large overhead

--- ingestion-beam/src/main/java/com/mozilla/telemetry/PioneerBenchmarkGenerator.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/PioneerBenchmarkGenerator.java
@@ -1,36 +1,38 @@
 package com.mozilla.telemetry;
 
-import com.mozilla.telemetry.options.SinkOptions;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class PioneerBenchmarkGenerator {
+
   // create a temporary directory
   // generate a keyfile metadata
   // encode all of the messages
   // upload into a temporary directory (or named directory)
+  final static ObjectMapper mapper = new ObjectMapper();
+
+  public static Optional<String> transform(String data) {
+    try {
+      JsonNode node = mapper.readTree(data);
+      return Optional.of(node.get("attributeMap").get("document_namespace").toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+  }
 
   public static void main(final String[] args) {
-    run(args);
+    try (Stream<String> stream = Files.lines(Paths.get("document_sample.ndjson"))) {
+
+      Files.write(Paths.get("output.ndjson"), (Iterable<String>) stream.map(s -> transform(s))
+          .filter(Optional::isPresent).map(Optional::get)::iterator);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
-
-  public interface PioneerBenchmarkOptions extends SinkOptions {
-  }
-
-  public static PipelineResult run(final String[] args) {
-    PioneerBenchmarkOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
-        .as(PioneerBenchmarkOptions.class);
-
-    // parsed sink options
-    SinkOptions.Parsed sinkOptions = SinkOptions.parseSinkOptions(options);
-
-    Pipeline p = Pipeline.create(sinkOptions);
-
-    p.apply(options.getInputType().read(sinkOptions)) //
-        .apply(options.getOutputType().write(sinkOptions));
-    return p.run();
-
-  }
-
 }
