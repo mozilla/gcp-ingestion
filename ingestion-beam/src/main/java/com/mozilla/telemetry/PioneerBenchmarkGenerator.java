da3cd1d31f11664ba00bd018b88e0a442118ea62
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-28 16:03:16 -0700

    Add metadata and keys

--- ingestion-beam/src/main/java/com/mozilla/telemetry/PioneerBenchmarkGenerator.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/PioneerBenchmarkGenerator.java
@@ -1,13 +1,16 @@
 package com.mozilla.telemetry;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Charsets;
 import com.mozilla.telemetry.util.Json;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.PublicKey;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
@@ -35,9 +38,17 @@ public class PioneerBenchmarkGenerator {
     return Json.asString(node).getBytes(Charsets.UTF_8);
   }
 
-  public static Optional<String> transform(String data, PublicKey key) {
+  public static Optional<PubsubMessage> parsePubsub(String data) {
     try {
-      PubsubMessage message = Json.readPubsubMessage(data);
+      return Optional.of(Json.readPubsubMessage(data));
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+  }
+
+  public static Optional<String> transform(PubsubMessage message, PublicKey key) {
+    try {
       PubsubMessage encryptedMessage = new PubsubMessage(encrypt(message.getPayload(), key),
           message.getAttributeMap());
       return Optional.of(Json.asString(encryptedMessage));
@@ -47,18 +58,46 @@ public class PioneerBenchmarkGenerator {
     }
   }
 
-  public static void main(final String[] args) throws JoseException {
+  /** Read in documents in the shape of Pubsub ndjson files and encrypt using
+   * Pioneer parameters and envelope. Write out the relevant metadata files for
+   * the single key in use. */
+  public static void main(final String[] args) throws JoseException, IOException {
+    Path inputPath = Paths.get("document_sample.ndjson");
+    Path outputPath = Paths.get("pioneer_benchmark_data.ndjson");
+    Path keyPath = Paths.get("pioneer_benchmark_key.ndjson");
+    Path metadataPath = Paths.get("pioneer_benchmark_metadata.json");
+
     EllipticCurveJsonWebKey key = EcJwkGenerator.generateJwk(EllipticCurves.P256);
-    // write the key to disk
-    // write the metadata file
+    HashSet<String> namespaces = new HashSet<String>();
 
-    try (Stream<String> stream = Files.lines(Paths.get("document_sample.ndjson"))) {
-
-      Files.write(Paths.get("pioneer_benchmark.ndjson"),
-          (Iterable<String>) stream.map(s -> transform(s, key.getPublicKey()))
-              .filter(Optional::isPresent).map(Optional::get)::iterator);
+    // runs in ~2:30 min
+    try (Stream<String> stream = Files.lines(inputPath)) {
+      // side-effects to get the set of attributes
+      Files.write(outputPath, (Iterable<String>) stream.map(PioneerBenchmarkGenerator::parsePubsub)
+          .filter(Optional::isPresent).map(Optional::get).map(message -> {
+            // side-effects and side-input
+            namespaces.add(message.getAttribute("document_namespace"));
+            return transform(message, key.getPublicKey());
+          }).filter(Optional::isPresent).map(Optional::get)::iterator);
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    // write out the key
+    Files.write(keyPath, key.toJson().getBytes(Charsets.UTF_8));
+
+    // write out the metadata
+    ArrayNode metadata = mapper.createArrayNode();
+    for (String namespace : namespaces) {
+      ObjectNode node = mapper.createObjectNode();
+      node.put("private_key_id", namespace);
+      // TODO: write this to the appropriate location like gcs, if relevant
+      node.put("private_key_uri", keyPath.toString());
+      // empty value
+      node.put("kms_resource_id", "");
+      metadata.add(node);
+    }
+    Files.write(metadataPath, metadata.toPrettyString().getBytes(Charsets.UTF_8));
+
   }
 }
