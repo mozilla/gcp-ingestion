67bb5fb5d28564273959a15604cb2f871edb613c
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-05-08 11:13:43 -0700

    Add keystore test and validate before assignment

--- ingestion-beam/src/main/java/com/mozilla/telemetry/util/KeyStore.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/util/KeyStore.java
@@ -47,13 +47,12 @@ public class KeyStore {
   }
 
   private KeyStore(String metadataLocation, boolean kmsEnabled) {
-    this.metadataLocation = metadataLocation;
-    this.kmsEnabled = kmsEnabled;
-
     // throw immediately if the keyStore is misconfigured
     if (metadataLocation == null) {
       throw new IllegalArgumentException("Metadata location is missing.");
     }
+    this.metadataLocation = metadataLocation;
+    this.kmsEnabled = kmsEnabled;
   }
 
   @VisibleForTesting
