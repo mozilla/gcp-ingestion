22779291db884e19da319ce8976ada8261599284
Jeff Klukas <jeff@klukas.net>
2020-08-14 15:02:30 -0400

    Support anonId updates

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseLogEntry.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseLogEntry.java
@@ -1,5 +1,6 @@
 package com.mozilla.telemetry.decoder;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mozilla.telemetry.ingestion.core.Constant.Attribute;
 import com.mozilla.telemetry.util.Json;
@@ -25,7 +26,7 @@ import org.apache.beam.sdk.values.TypeDescriptor;
 public class ParseLogEntry
     extends PTransform<PCollection<PubsubMessage>, PCollection<PubsubMessage>> {
 
-  private final Counter logEntryPayload = Metrics.counter(ParseLogEntry.class,
+  private final Counter countLogEntryPayload = Metrics.counter(ParseLogEntry.class,
       "log_entry_payload");
 
   public static ParseLogEntry of() {
@@ -40,7 +41,7 @@ public class ParseLogEntry
         // server rather than a LogEntry from Cloud Logging, so we return immediately.
         return m;
       }
-      logEntryPayload.inc();
+      countLogEntryPayload.inc();
       ObjectNode logEntry;
       HashMap<String, String> attributes = new HashMap<>();
       ObjectNode json = Json.createObjectNode();
@@ -49,27 +50,33 @@ public class ParseLogEntry
       } catch (IOException e) {
         throw new UncheckedIOException(e);
       }
-      Optional.ofNullable(logEntry.path("insertId").textValue())
-          .ifPresent(id -> attributes.put(Attribute.DOCUMENT_ID,
-              UUID.nameUUIDFromBytes(id.getBytes(StandardCharsets.UTF_8)).toString()));
+      JsonNode fields = logEntry.path("jsonPayload").path("Fields");
+      String documentId = Optional.ofNullable(logEntry.path("insertId").textValue())
+          .map(id -> attributes.put(Attribute.DOCUMENT_ID,
+              UUID.nameUUIDFromBytes(id.getBytes(StandardCharsets.UTF_8)).toString()))
+          .orElse(UUID.randomUUID().toString());
+      attributes.put(Attribute.DOCUMENT_ID, documentId);
       Optional.ofNullable(logEntry.path("receiveTimestamp").textValue())
           .ifPresent(v -> attributes.put(Attribute.SUBMISSION_TIMESTAMP, v));
-      Optional.ofNullable(logEntry.path("jsonPayload").path("Fields").path("userAgent").textValue())
+      Optional.ofNullable(fields.path("userAgent").textValue())
           .ifPresent(v -> attributes.put(Attribute.USER_AGENT, v));
-      String ecosystemAnonId = logEntry.path("jsonPayload").path("Fields").path("ecosystemAnonId")
-          .textValue();
-      String event = logEntry.path("jsonPayload").path("Fields").path("event").asText();
+      String event = fields.path("event").asText();
+      String ecosystemAnonId = fields.path("ecosystemAnonId").textValue();
+      attributes.put(Attribute.URI,
+          String.format("/submit/firefox-accounts/account-ecosystem/1/%s", documentId));
       // TODO: Parse geo info
       if ("oauth.token.created".equals(event) && ecosystemAnonId != null) {
-        attributes.put(Attribute.URI,
-            String.format("/submit/firefox-accounts/account-ecosystem/1/%s",
-                attributes.get(Attribute.DOCUMENT_ID)));
         json.put("ecosystem_anon_id", ecosystemAnonId);
-        return new PubsubMessage(Json.asBytes(json), attributes);
+      } else if ("account.updateEcosystemAnonId.complete".equals(event)) {
+        Optional.ofNullable(logEntry.path("next").textValue())
+            .ifPresent(v -> json.put("ecosystem_anon_id", v));
+        Optional.ofNullable(logEntry.path("current").textValue())
+            .ifPresent(v -> json.set("previous_ecosystem_anon_ids", Json.createArrayNode().add(v)));
       } else {
         throw new IllegalArgumentException(
             "Received an unexpected payload without submission_timestamp");
       }
+      return new PubsubMessage(Json.asBytes(json), attributes);
     }));
   }
 }
