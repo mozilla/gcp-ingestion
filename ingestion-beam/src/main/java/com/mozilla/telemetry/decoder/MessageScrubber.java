8760be3aab52f2cdab97200f4a10069c976b443e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-03-17 16:48:56 -0700

    Bug 1698579 - Ignore org-mozilla-firefox-betb

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
@@ -52,6 +52,7 @@ public class MessageScrubber {
       .put("org-privacywall-browser", "1691468") //
       .put("org-mozilla-ios-lockbox-credentialprovider", "1695728") //
       .put("com-zibb-browser", "1698576") //
+      .put("org-mozilla-firefox-betb", "1698579") //
       .build();
 
   private static final Map<String, String> IGNORED_TELEMETRY_DOCTYPES = ImmutableMap
