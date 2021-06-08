e5dc0fa46a940ec047a39369d05a7d132213382d
Daniel Thorn <dthorn@mozilla.com>
2021-06-08 12:30:58 -0700

    Depend on beam bom for GCP instead of libraries-bom (#1713)

--- ingestion-beam/src/test/java/com/mozilla/telemetry/integration/BeamDependenciesIntegrationTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/integration/BeamDependenciesIntegrationTest.java
@@ -20,19 +20,14 @@ public class BeamDependenciesIntegrationTest {
   public void checkVersions() throws Exception {
     final String beamVersion = System.getProperty("beam.version");
     final Pom beamCore = getPom("org.apache.beam", "beam-sdks-java-core", beamVersion);
-    final Pom beamGcpIO = getPom("org.apache.beam", "beam-sdks-java-io-google-cloud-platform",
-        beamVersion);
 
     final Map<String, Optional<String>> expectedVersions = ImmutableMap.of(//
         "avro.version", beamCore.getVersion("org.apache.avro", "avro"), //
-        "jackson.version", beamCore.getVersion("com.fasterxml.jackson.core", "jackson-core"), //
-        "googleCloudLibraries.version", beamGcpIO.getVersion("com.google.cloud", "libraries-bom"));
+        "jackson.version", beamCore.getVersion("com.fasterxml.jackson.core", "jackson-core"));
 
     final Map<String, Optional<String>> actualVersions = ImmutableMap.of(//
         "avro.version", Optional.of(System.getProperty("avro.version")), //
-        "jackson.version", Optional.of(System.getProperty("jackson.version")), //
-        "googleCloudLibraries.version",
-        Optional.of(System.getProperty("googleCloudLibraries.version")));
+        "jackson.version", Optional.of(System.getProperty("jackson.version")));
 
     assertEquals(expectedVersions, actualVersions);
   }
