fb01b602b85d95b1d3e03ea64bf58aba55e78899
Jeff Klukas <jeff@klukas.net>
2021-06-25 11:53:25 -0400

    Tests for WithCurrentTimestamp

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/WithCurrentTimestamp.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/WithCurrentTimestamp.java
@@ -1,13 +1,13 @@
 package com.mozilla.telemetry.transforms;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.joda.time.Instant;
 
-public class WithCurrentTimestamp<T>
-    extends PTransform<PCollection<T>, PCollection<T>> {
+public class WithCurrentTimestamp<T> extends PTransform<PCollection<T>, PCollection<T>> {
 
   public static <T> WithCurrentTimestamp<T> of() {
     return new WithCurrentTimestamp<>();
@@ -16,21 +16,27 @@ public class WithCurrentTimestamp<T>
   private WithCurrentTimestamp() {
   }
 
+  @VisibleForTesting
+  static Instant adjustedTimestamp(Instant inputTimestamp) {
+    Instant timestamp = new Instant();
+    if (timestamp.isBefore(inputTimestamp)) {
+      // A DoFn will throw an error if it ever tries to shift an element backwards in time,
+      // which could put it behind the watermark and make it considered "late", so we maintain
+      // the inputTimestamp in cases where it appears to be ahead of the current server time.
+      // See
+      // https://beam.apache.org/releases/javadoc/2.29.0/org/apache/beam/sdk/transforms/WithTimestamps.html#getAllowedTimestampSkew--
+      return inputTimestamp;
+    } else {
+      return timestamp;
+    }
+  }
+
   private static class Fn<T> extends DoFn<T, T> {
 
     @ProcessElement
     public void processElement(@Element T message, @Timestamp Instant inputTimestamp,
         OutputReceiver<T> out) {
-      Instant timestamp = new Instant();
-      if (timestamp.isBefore(inputTimestamp)) {
-        // A DoFn will throw an error if it ever tries to shift an element backwards in time,
-        // which could put it behind the watermark and make it considered "late", so we maintain
-        // the inputTimestamp in cases where it appears to be ahead of the current server time.
-        // See
-        // https://beam.apache.org/releases/javadoc/2.29.0/org/apache/beam/sdk/transforms/WithTimestamps.html#getAllowedTimestampSkew--
-        timestamp = inputTimestamp;
-      }
-      out.outputWithTimestamp(message, timestamp);
+      out.outputWithTimestamp(message, adjustedTimestamp(inputTimestamp));
     }
   }
 
