d47312e4309ca61fcf8ba1f56a972ad48d500700
Jeff Klukas <jklukas@mozilla.com>
2021-09-29 13:44:24 -0400

    Bug 1728191 Re-enable search terms ingestion (#1840)
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1728191

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
@@ -199,13 +199,6 @@ public class MessageScrubber {
       });
     }
 
-    if (bug1712850Affected(attributes)) {
-      if (json.hasNonNull("search_query") || json.hasNonNull("matched_keywords")) {
-        json.put("search_query", "");
-        json.put("matched_keywords", "");
-        markBugCounter("1712850");
-      }
-    }
   }
 
   /**
@@ -316,10 +309,4 @@ public class MessageScrubber {
         && attributes.get(Attribute.APP_VERSION).matches("^([0-9]|[0-2][0-7])\\..*"); // <= 27
   }
 
-  // See bug 1712850
-  @VisibleForTesting
-  static boolean bug1712850Affected(Map<String, String> attributes) {
-    return "contextual-services".equals(attributes.get(Attribute.DOCUMENT_NAMESPACE))
-        && "quicksuggest-impression".equals(attributes.get(Attribute.DOCUMENT_TYPE));
-  }
 }
