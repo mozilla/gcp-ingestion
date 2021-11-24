a0bc09ef860500c65da6540e62c2bd5091de3352
Jeff Klukas <jeff@klukas.net>
2021-11-24 14:57:49 -0500

    Bug 1742172 Support override_attributes metadata
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1742172
    
    A coordinated change in mozilla-pipeline-schemas to come.

--- ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/SanitizeAttributesTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/decoder/SanitizeAttributesTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import com.mozilla.telemetry.ingestion.core.Constant.Attribute;
 import com.mozilla.telemetry.util.TestWithDeterministicJson;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.testing.PAssert;
@@ -17,42 +18,82 @@ import org.junit.Test;
 
 public class SanitizeAttributesTest extends TestWithDeterministicJson {
 
+  static final byte[] EMPTY_JSON = "{}".getBytes(StandardCharsets.UTF_8);
+
   @Rule
   public final transient TestPipeline pipeline = TestPipeline.create();
 
   @Test
   public void testNoop() {
-    Map<String, String> attributes = ImmutableMap.<String, String>builder()
+    Map<String, String> inputAttributes = ImmutableMap.<String, String>builder()
         .put(Attribute.DOCUMENT_NAMESPACE, "test") //
         .put(Attribute.DOCUMENT_TYPE, "test") //
         .put(Attribute.DOCUMENT_VERSION, "1") //
         .put(Attribute.SUBMISSION_TIMESTAMP, "2021-01-01T12:00:00.123456Z") //
         .put(Attribute.GEO_CITY, "Toronto") //
         .build();
-    PCollection<String> result = pipeline //
-        .apply(Create.of(new PubsubMessage("{}".getBytes(StandardCharsets.UTF_8), attributes))) //
+
+    // Output should match the input exactly.
+    Map<String, String> expected = new HashMap<>(inputAttributes);
+
+    PCollection<Map<String, String>> result = pipeline //
+        .apply(Create.of(new PubsubMessage(EMPTY_JSON, inputAttributes))) //
         .apply(SanitizeAttributes.of("schemas.tar.gz"))
-        .apply(MapElements.into(TypeDescriptors.strings())
-            .via(message -> message.getAttribute(Attribute.SUBMISSION_TIMESTAMP)));
-    PAssert.that(result).containsInAnyOrder("2021-01-01T12:00:00.123456Z");
+        .apply(MapElements
+            .into(TypeDescriptors.maps(TypeDescriptors.strings(), TypeDescriptors.strings()))
+            .via(PubsubMessage::getAttributeMap));
+    PAssert.that(result).containsInAnyOrder(expected);
     pipeline.run();
   }
 
   @Test
   public void testSubmissionTimestampGranularity() {
     // Depends on custom doctype injected in bin/download-schemas
-    Map<String, String> attributes = ImmutableMap.<String, String>builder()
+    Map<String, String> inputAttributes = ImmutableMap.<String, String>builder()
         .put(Attribute.DOCUMENT_NAMESPACE, "test") //
         .put(Attribute.DOCUMENT_TYPE, "seconds") //
         .put(Attribute.DOCUMENT_VERSION, "1") //
-        .put("submission_timestamp", "2021-01-01T12:00:00.123456Z") //
+        .put(Attribute.SUBMISSION_TIMESTAMP, "2021-01-01T12:00:00.123456Z") //
+        .put(Attribute.GEO_CITY, "Toronto") //
         .build();
-    PCollection<String> result = pipeline //
-        .apply(Create.of(new PubsubMessage("{}".getBytes(StandardCharsets.UTF_8), attributes))) //
+
+    Map<String, String> expected = new HashMap<>(inputAttributes);
+    expected.put(Attribute.SUBMISSION_TIMESTAMP, "2021-01-01T12:00:00Z");
+
+    PCollection<Map<String, String>> result = pipeline //
+        .apply(Create.of(new PubsubMessage(EMPTY_JSON, inputAttributes))) //
         .apply(SanitizeAttributes.of("schemas.tar.gz"))
-        .apply(MapElements.into(TypeDescriptors.strings())
-            .via(message -> message.getAttribute(Attribute.SUBMISSION_TIMESTAMP)));
-    PAssert.that(result).containsInAnyOrder("2021-01-01T12:00:00Z");
+        .apply(MapElements
+            .into(TypeDescriptors.maps(TypeDescriptors.strings(), TypeDescriptors.strings()))
+            .via(PubsubMessage::getAttributeMap));
+    PAssert.that(result).containsInAnyOrder(expected);
+    pipeline.run();
+  }
+
+  @Test
+  public void testOverrideAttributes() {
+    // Depends on custom doctype injected in bin/download-schemas
+    Map<String, String> inputAttributes = ImmutableMap.<String, String>builder()
+        .put(Attribute.DOCUMENT_NAMESPACE, "test") //
+        .put(Attribute.DOCUMENT_TYPE, "attribute-overrides") //
+        .put(Attribute.DOCUMENT_VERSION, "1") //
+        .put(Attribute.SUBMISSION_TIMESTAMP, "2021-01-01T12:00:00.123456Z") //
+        .put(Attribute.GEO_CITY, "Toronto") //
+        .put(Attribute.NORMALIZED_CHANNEL, "bizarreCustomValue") //
+        .build();
+
+    Map<String, String> expected = new HashMap<>(inputAttributes);
+    expected.remove(Attribute.GEO_CITY);
+    expected.put(Attribute.NORMALIZED_CHANNEL, "nightly");
+    expected.put("new_attr", "injected_value");
+
+    PCollection<Map<String, String>> result = pipeline //
+        .apply(Create.of(new PubsubMessage(EMPTY_JSON, inputAttributes))) //
+        .apply(SanitizeAttributes.of("schemas.tar.gz"))
+        .apply(MapElements
+            .into(TypeDescriptors.maps(TypeDescriptors.strings(), TypeDescriptors.strings()))
+            .via(PubsubMessage::getAttributeMap));
+    PAssert.that(result).containsInAnyOrder(expected);
     pipeline.run();
   }
 }
