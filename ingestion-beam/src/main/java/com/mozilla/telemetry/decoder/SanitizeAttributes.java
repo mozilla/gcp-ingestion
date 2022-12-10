67a48b91c2f45e5dcd4051c7a5396ebc15625500
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2022-12-10 13:04:58 +0100

    Optimize schemas loading in the Decoder
    Use shared singleton instances of SchemaStores initialized in the `DoFn.Setup` instead of in the main processing method of the `ParsePayload` transform. This lowers memory utilization and makes the job more stable when scaling up with higher number of worker threads (for example with Streaming Engine enabled).

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/SanitizeAttributes.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/SanitizeAttributes.java
@@ -3,8 +3,8 @@ package com.mozilla.telemetry.decoder;
 import com.mozilla.telemetry.ingestion.core.Constant.Attribute;
 import com.mozilla.telemetry.ingestion.core.schema.PipelineMetadataStore;
 import com.mozilla.telemetry.ingestion.core.schema.PipelineMetadataStore.PipelineMetadata;
+import com.mozilla.telemetry.schema.SchemaStoreSingletonFactory;
 import com.mozilla.telemetry.transforms.PubsubConstraints;
-import com.mozilla.telemetry.util.BeamFileInputStream;
 import com.mozilla.telemetry.util.Time;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
@@ -12,9 +12,9 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
-import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 
 /**
@@ -38,7 +38,7 @@ public class SanitizeAttributes
 
   @Override
   public PCollection<PubsubMessage> expand(PCollection<PubsubMessage> input) {
-    return input.apply(MapElements.via(fn));
+    return input.apply(ParDo.of(new Fn()));
   }
 
   ////////
@@ -47,17 +47,17 @@ public class SanitizeAttributes
     this.schemasLocation = schemasLocation;
   }
 
-  private class Fn extends SimpleFunction<PubsubMessage, PubsubMessage> {
+  private class Fn extends DoFn<PubsubMessage, PubsubMessage> {
 
-    @Override
-    public PubsubMessage apply(PubsubMessage message) {
+    @Setup
+    public void setup() {
+      pipelineMetadataStore = SchemaStoreSingletonFactory.getPipelineMetadataStore(schemasLocation);
+    }
+
+    @ProcessElement
+    public void processElement(@Element PubsubMessage message, OutputReceiver<PubsubMessage> out) {
       message = PubsubConstraints.ensureNonNull(message);
       Map<String, String> attributes = new HashMap<>(message.getAttributeMap());
-
-      if (pipelineMetadataStore == null) {
-        pipelineMetadataStore = PipelineMetadataStore.of(schemasLocation,
-            BeamFileInputStream::open);
-      }
 
       final PipelineMetadata meta = pipelineMetadataStore.getSchema(attributes);
 
@@ -82,10 +82,7 @@ public class SanitizeAttributes
         });
       }
 
-      return new PubsubMessage(message.getPayload(), attributes);
+      out.output(new PubsubMessage(message.getPayload(), attributes));
     }
   }
-
-  private final Fn fn = new Fn();
-
 }
---
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/schema/SchemaStoreSingletonFactory.java
@@ -0,0 +1,55 @@
+package com.mozilla.telemetry.schema;
+
+import com.mozilla.telemetry.ingestion.core.schema.JSONSchemaStore;
+import com.mozilla.telemetry.ingestion.core.schema.PipelineMetadataStore;
+import com.mozilla.telemetry.util.BeamFileInputStream;
+
+/**
+ * Factory class for getting singleton instances of SchemaStores.
+ *
+ * <p>Beam can run up to 500 threads per worker
+ * (see https://cloud.google.com/dataflow/docs/guides/troubleshoot-oom#dofn)
+ * By using singleton instances of SchemaStores we can speed up worker startup a bit and
+ * decrease memory utilization.
+ *
+ * <p>`schemasLocation` parameter in the factory methods here is used only during object creation.
+ * This is fine here since the value is not changing during runtime of the job.
+ */
+public class SchemaStoreSingletonFactory {
+
+  private static volatile JSONSchemaStore jsonSchemaStore;
+  private static volatile PipelineMetadataStore pipelineMetadataStore;
+
+  private SchemaStoreSingletonFactory() {
+  }
+
+  /**
+   * Returns a singleton instance of {@link JSONSchemaStore}.
+   */
+  public static JSONSchemaStore getJsonSchemaStore(String schemasLocation) {
+    if (jsonSchemaStore == null) {
+      synchronized (SchemaStoreSingletonFactory.class) {
+        if (jsonSchemaStore == null) {
+          jsonSchemaStore = JSONSchemaStore.of(schemasLocation, BeamFileInputStream::open);
+        }
+      }
+    }
+    return jsonSchemaStore;
+  }
+
+  /**
+   * Returns a singleton instance of {@link PipelineMetadataStore}.
+   */
+  public static PipelineMetadataStore getPipelineMetadataStore(String schemasLocation) {
+    if (pipelineMetadataStore == null) {
+      synchronized (SchemaStoreSingletonFactory.class) {
+        if (pipelineMetadataStore == null) {
+          pipelineMetadataStore = PipelineMetadataStore.of(schemasLocation,
+              BeamFileInputStream::open);
+        }
+      }
+    }
+    return pipelineMetadataStore;
+  }
+
+}
