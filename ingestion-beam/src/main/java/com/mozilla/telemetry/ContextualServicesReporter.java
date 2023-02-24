d664ef1ab88c876b177dd62bbf8a32dbbf7729b3
Chelsea Troy <chelsea.dommert@gmail.com>
2023-02-24 13:10:33 -0600

    More formatting

--- ingestion-beam/src/main/java/com/mozilla/telemetry/ContextualServicesReporter.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/ContextualServicesReporter.java
@@ -4,13 +4,13 @@ import com.google.common.collect.ImmutableSet;
 import com.mozilla.telemetry.contextualservices.ContextualServicesReporterOptions;
 import com.mozilla.telemetry.contextualservices.FilterByDocType;
 import com.mozilla.telemetry.contextualservices.SponsoredInteraction;
-import com.mozilla.telemetry.contextualservices.VerifyMetadata;
 import com.mozilla.telemetry.contextualservices.ParseReportingUrl;
 import com.mozilla.telemetry.contextualservices.EmitCounters;
 import com.mozilla.telemetry.contextualservices.LabelSpikes;
 import com.mozilla.telemetry.contextualservices.TelemetryEventType;
 import com.mozilla.telemetry.contextualservices.AggregateImpressions;
 import com.mozilla.telemetry.contextualservices.SendRequest;
+import com.mozilla.telemetry.contextualservices.VerifyMetadata;
 import com.mozilla.telemetry.transforms.DecompressPayload;
 import com.mozilla.telemetry.util.Time;
 import java.util.ArrayList;
@@ -129,7 +129,11 @@ public class ContextualServicesReporter extends Sink {
             .contains(interaction.getDerivedDocumentType())));
 
     PCollectionList.of(aggregatedGenuineImpressions).and(aggregatedPossiblyFraudulentImpressions)
-        .and(clicksCountedByContextId).and(impressionsCountedByContextId).and(unaggregated).apply(Flatten.pCollections())
+        .and(clicksCountedByContextId)
+        .and(impressionsCountedByContextId)
+        .and(unaggregated)
+        .apply(Flatten.pCollections()
+        )
         .apply(SendRequest.of(options.getReportingEnabled(), options.getLogReportingUrls()))
         .failuresTo(errorCollections);
 
