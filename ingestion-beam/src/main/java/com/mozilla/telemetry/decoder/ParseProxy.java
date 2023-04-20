8dc4986c199ea11e31289812693bb134c54d1b08
Wesley Dawson <whd@mozilla.com>
2023-04-20 16:22:00 -0700

    waatt

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseProxy.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseProxy.java
@@ -114,7 +114,8 @@ public class ParseProxy extends PTransform<PCollection<PubsubMessage>, PCollecti
                 }
                 xff.remove(xff.size() - 1); // remove last entry when proxy is detected
                 if (ProxyMatcher.ProxySource.GOOGLE == pt) {
-                  xff.remove(xff.size() - 1); // remove an additional entry for google proxy
+                  break;
+                  // xff.remove(xff.size() - 1); // remove an additional entry for google proxy
                 } else {
                   break;
                 }
