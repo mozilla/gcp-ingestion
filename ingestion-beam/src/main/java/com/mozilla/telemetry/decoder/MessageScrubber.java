4d6e83edceee3d408dbadf504efc547754b1023b
Leli Schiestl <lschiestl@mozilla.com>
2023-06-13 12:18:29 +0200

    Bug 1834366 - remove namespace

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
@@ -100,6 +100,7 @@ public class MessageScrubber {
       .put("com-readboy-firefox", "1833203") //
       .put("website-carbon-ios-browser", "1833204") //
       .put("viu-politica", "1826535") //
+      .put("com-freespoke-app", "1834366") //
       .build();
 
   private static final Map<String, String> IGNORED_TELEMETRY_DOCTYPES = ImmutableMap
