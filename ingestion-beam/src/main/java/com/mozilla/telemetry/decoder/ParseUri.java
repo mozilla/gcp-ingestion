2c05053a254a29e625b40d2c10c5a89b01fefeef
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2024-10-10 15:23:46 +0200

    reformat

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUri.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUri.java
@@ -288,8 +288,8 @@ public class ParseUri {
         putBoolPerCode(ImmutableMap.of("set_default", 2)), //
         putString("download_ip"), putString("attribution"),
         putIntegerAsString("profile_cleanup_prompt"), putBool("profile_cleanup_requested"),
-        putString("distribution_id"), putString("distribution_version"), putInteger("windows_ubr"),
-        putString("stub_build_id"));
+        putString("distribution_id"), putString("distribution_version"),
+        putInteger("windows_ubr"), putString("stub_build_id"));
 
     private static BiConsumer<String, ObjectNode> ignore() {
       return (value, payload) -> {
