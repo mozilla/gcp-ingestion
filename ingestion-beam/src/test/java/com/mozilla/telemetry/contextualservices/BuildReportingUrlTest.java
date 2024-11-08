d653548082ffd5fa49e6e2a3987a9b87eef62792
Can Berk Güder <cbguder@mozilla.com>
2024-11-08 15:05:12 -0800

    Remove unneeded test pipeline

--- ingestion-beam/src/test/java/com/mozilla/telemetry/contextualservices/BuildReportingUrlTest.java
+++ ingestion-beam/src/test/java/com/mozilla/telemetry/contextualservices/BuildReportingUrlTest.java
@@ -1,15 +1,10 @@
 package com.mozilla.telemetry.contextualservices;
 
-import org.apache.beam.sdk.testing.TestPipeline;
-import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThrows;
 
 public class BuildReportingUrlTest {
-
-  @Rule
-  public final transient TestPipeline pipeline = TestPipeline.create();
 
   @Test
   public void testDuplicateQueryKeys() {
@@ -18,4 +13,5 @@ public class BuildReportingUrlTest {
         () -> new BuildReportingUrl("https://example.com?foo=bar&foo=bar")
     );
   }
+
 }
