d08f5892b670a1d0f55d3b427300d0ce268cbafd
Antonio Borondo <aborondo@mozilla.com>
2026-02-23 15:19:15 +0100

    Bug 2017610 - Add metric to record Desktop Launcher installation status

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUri.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/ParseUri.java
@@ -248,7 +248,7 @@ public class ParseUri {
         .compile(String.format("^v(?<%s>[6-9]|1[0-9])(-(?<%s>\\d+))?$", VERSION, FUNNELCAKE));
 
     public static final Map<String, Integer> SUFFIX_LENGTH = ImmutableMap.of("6", 36, "7", 37, "8",
-        39, "9", 41, "10", 43, "11", 44, "12", 45);
+        39, "9", 41, "10", 43, "11", 44, "12", 45, "13", 46);
 
     /**
      * NOTE: When adding new fields here, the version must be incremented on the client.
@@ -304,7 +304,8 @@ public class ParseUri {
         putString("distribution_id"), putString("distribution_version"), //
         putInteger("windows_ubr"), putString("stub_build_id"), //
         putString("launched_by"), //
-        putInteger("download_requests_blocked_by_server"));
+        putInteger("download_requests_blocked_by_server"), //
+        putInteger("desktop_launcher_status"));
 
     private static BiConsumer<String, ObjectNode> ignore() {
       return (value, payload) -> {
