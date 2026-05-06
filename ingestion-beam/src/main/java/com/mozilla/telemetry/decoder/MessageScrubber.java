c54deb26cefb013a28fe9045253a0022ab3443c1
Winnie Chan <10429026+wwyc@users.noreply.github.com>
2026-05-06 13:22:49 -0700

    removed empty spaces

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/MessageScrubber.java
@@ -168,7 +168,7 @@ public class MessageScrubber {
       .<String, String>builder() //
       .put("com-feifan", "2010623") //
       .put("org-global-g", "2024379") //
-      .put("org-global-wxc", "2037560") //  
+      .put("org-global-wxc", "2037560") //
       .build();
 
   private static final Map<String, String> IGNORED_TELEMETRY_DOCTYPES = ImmutableMap
