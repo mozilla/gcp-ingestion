8a7d5150bb2eb062259dec6ce9b43fd1c81ab730
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2026-07-02 16:24:33 +0200

    trigger java workflow

--- ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/DecoderOptions.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/decoder/DecoderOptions.java
@@ -21,7 +21,7 @@ public interface DecoderOptions extends SinkOptions, PipelineOptions {
   void setGeoCityDatabase(String value);
 
   @Description("Path (local or gs://) to newline-delimited text file listing city names to allow"
-      + "in geoCity information; cities not in the list are considered too small to ensure"
+      + " in geoCity information; cities not in the list are considered too small to ensure"
       + " user anonymity so we won't report geoCity in that case."
       + " If not specified, no limiting is performed and we always report valid geoCity values.")
   String getGeoCityFilter();
